054d914ac6bdc350adb1d28350914e522bfa7d6b
caaaden <121105930+caaaden@users.noreply.github.com>
2024-06-27 09:38:44 +0900

    [Bronze I] Title: Solitaire, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11323. Solitaire/Solitaire.cc
+++ 백준/Bronze/11323. Solitaire/Solitaire.cc
@@ -2,7 +2,6 @@
 
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
-using i64 = long long;
 
 int main() {
     fastio;
@@ -12,10 +11,10 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        vector<i64> v(6);
+        vector<int> v(6);
         for (auto& e : v) cin >> e;
-        i64 sum = 0;
-        i64 sumOfSum = 0;
+        int sum = 0;
+        int sumOfSum = 0;
         for (int i = 0, cnt = 0; cnt < 6; ++i) {
             if (sum+v[i%6] <= n) {
                 if (cnt) cnt = 0;
