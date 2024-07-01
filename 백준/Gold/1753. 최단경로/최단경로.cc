594d8435c9f7dcb1b032a5c771e42ad29f86c548
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-01 13:23:49 +0900

    [Gold IV] Title: 최단경로, Time: 112 ms, Memory: 9112 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 using pii = pair<int, int>;
-using piii = pair<pair<int, int>, int>;
 
-int v, e;
+int v, e, s;
 const int MAX = 1e9;
-vector<int> dijkstra(int s, vector<vector<pii>>& g) {
+vector<int> dijkstra(vector<vector<pii>>& g) {
     vector<int> dist(v+1, MAX);
     priority_queue<pii, vector<pii>, greater<pii>> pq;
     dist[s] = 0;
@@ -20,7 +19,7 @@ vector<int> dijkstra(int s, vector<vector<pii>>& g) {
             int nextCost = cost + g[now][i].second;
             if (nextCost < dist[next]) {
                 dist[next] = nextCost;
-                pq.push({nextCost, next}); // 현재 next까지의 최단 거리, next 번호
+                pq.push({nextCost, next});
             }
         }
     }
@@ -30,51 +29,17 @@ vector<int> dijkstra(int s, vector<vector<pii>>& g) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     cin >> v >> e;
-    int s;
     cin >> s;
     vector<vector<pii>> graph(v+1);
-    for (int i = 0; i < e; ++i) {
-        int a, b, c;
+    for (int i = 0, a, b, c; i < e; ++i) {
         cin >> a >> b >> c;
         graph[a].push_back({b, c});
     }
-    
-    
-    // vector<piii> tmp(e);
-    // for (int i = 0; i < e; ++i) {
-    //     cin >> tmp[i].first.first >> tmp[i].first.second >> tmp[i].second;
-    // }
-    // sort(tmp.begin(), tmp.end(), [](piii& a, piii& b){
-    //     if (a.first.first == b.first.first) {
-    //         if (a.first.second == b.first.second) {
-    //             return a.second < b.second;
-    //         } else {
-    //             return a.first.second < b.first.second;
-    //         }
-    //     } else {
-    //         return a.first.first < b.first.first;
-    //     }
-    // });
-    // vector<piii> tmp2;
-    // tmp2.push_back(tmp[0]);
-    // for (int i = 1; i < tmp.size(); ++i) {
-    //     if (tmp[i].first == tmp[i-1].first) continue;
-    //     tmp2.push_back(tmp[i]);
-    // }
-    // for (int i = 0; i < tmp2.size(); ++i) {
-    //     int a = tmp2[i].first.first;
-    //     int b = tmp2[i].first.second;
-    //     int c = tmp2[i].second;
-    //     graph[a].push_back({b, c});
-    // }
-    vector<int> ans = dijkstra(s, graph);
+    vector<int> ans = dijkstra(graph);
     for (int i = 1; i < ans.size(); ++i) {
-        if (ans[i] == MAX) {
-            cout << "INF" << '\n';
-        } else {
-            cout << ans[i] << '\n';
-        }
+        if (ans[i] == MAX) cout << "INF" << '\n';
+        else cout << ans[i] << '\n';
     }
 }
