1e464a97010d22eae54269ccb39f7317f6c66116
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-01 14:01:36 +0900

    [Gold V] Title: 백도어, Time: 108 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.cc
+++ 백준/Gold/17396. 백도어/백도어.cc
@@ -1,49 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+using i64 = long long;
 using pii = pair<int, int>;
-using pli = pair<long long, int>;
+using pli = pair<i64, int>;
 
-const ll MAX = 999999999999999;
+const i64 MAX = LLONG_MAX;
 int n, m;
-vector<pii> graph[100000];
-ll dijkstra() {
-    vector<ll> dist(n, MAX);
+vector<vector<pii>> graph;
+i64 dijkstra() {
+    vector<i64> dist(n, MAX);
     priority_queue<pli, vector<pli>, greater<pli>> pq;
-    
     dist[0] = 0;
     pq.push({0, 0});
-    
-    while (!pq.empty()) {
-        ll cost = pq.top().first;
+    while (pq.size()) {
+        i64 cost = pq.top().first;
         int now = pq.top().second;
-        if (now == n-1) return dist[n-1];
         pq.pop();
         if (dist[now] < cost) continue;
         for (int i = 0; i < graph[now].size(); ++i) {
             int next = graph[now][i].first;
-            ll nextCost = cost + graph[now][i].second;
+            i64 nextCost = cost + graph[now][i].second;
             if (nextCost < dist[next]) {
                 dist[next] = nextCost;
                 pq.push({nextCost, next});
             }
         }
     }
-    return -1;
+    return dist[n-1] == MAX ? -1 : dist[n-1];
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     cin >> n >> m;
-    vector<int> cannotMove(n);
-    for (int i = 0; i < n; ++i) cin >> cannotMove[i]; // n-1번째 인덱스를 제외하고 cannotMove[i] == 1이면 그래프 연결 X
-    cannotMove[n-1] = 0;
+    graph.resize(n+1);
+    vector<int> isSighted(n);
+    for (int i = 0; i < n; ++i) cin >> isSighted[i];
+    isSighted[n-1] = 0;
     for (int i = 0; i < m; ++i) {
         int a, b, t;
         cin >> a >> b >> t;
-        if (cannotMove[a] || cannotMove[b]) continue;
+        if (isSighted[a] || isSighted[b]) continue;
         graph[a].push_back({b, t});
         graph[b].push_back({a, t});
     }
