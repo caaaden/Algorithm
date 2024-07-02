96bd141cf67ecb4384fe68d2223e7ddd78361243
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-03 06:12:25 +0900

    [Gold III] Title: 소문난 칠공주, Time: 168 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -9,31 +9,35 @@ vector<vector<char>> board(5, vector<char>(5));
 vector<vector<int>> board2(5, vector<int>(5));
 int cntY;
 int ans;
-vector<pii> selected;
 void dfs(int k, int idx) {
     if (k == 7) {
-        queue<pii> Q;
-        Q.push(selected[0]);
-        vector<vector<int>> vis(5, vector<int>(5));
-        vis[selected[0].first][selected[0].second] = 1;
-        int cnt = 0;
-        while (Q.size()) {
-            int nowX = Q.front().first;
-            int nowY = Q.front().second;
-            Q.pop();
-            cnt++;
-            for (int i = 0; i < 4; ++i) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-                if (nextX < 0 || nextX > 4 || nextY < 0 || nextY > 4) continue;
-                if (!board2[nextX][nextY]) continue;
-                if (vis[nextX][nextY]) continue;
-                vis[nextX][nextY] = 1;
-                Q.push({nextX, nextY});
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                if (!board2[i][j]) continue;
+                queue<pii> Q;
+                Q.push({i, j});
+                vector<vector<int>> vis(5, vector<int>(5));
+                vis[i][j] = 1;
+                int cnt = 0;
+                while (Q.size()) {
+                    int nowX = Q.front().first;
+                    int nowY = Q.front().second;
+                    Q.pop();
+                    cnt++;
+                    for (int dir = 0; dir < 4; ++dir) {
+                        int nextX = nowX + dx[dir];
+                        int nextY = nowY + dy[dir];
+                        if (nextX < 0 || nextX > 4 || nextY < 0 || nextY > 4) continue;
+                        if (!board2[nextX][nextY]) continue;
+                        if (vis[nextX][nextY]) continue;
+                        vis[nextX][nextY] = 1;
+                        Q.push({nextX, nextY});
+                    }
+                }
+                ans += cnt == 7;
+                return;
             }
         }
-        ans += cnt == 7;
-        return;
     }
     for (int i = idx; i < 25; ++i) {
         int x = i / 5;
@@ -43,11 +47,9 @@ void dfs(int k, int idx) {
             cntY--;
             continue;
         }
-        selected.push_back({x, y});
         board2[x][y] = 1;
         dfs(k+1, i+1);
         board2[x][y] = 0;
-        selected.pop_back();
         if (board[x][y] == 'Y') cntY--;
     }
 }
