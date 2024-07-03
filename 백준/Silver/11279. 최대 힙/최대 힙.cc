7b4983d10b74fab175ddcd4722684fc2e6a605fe
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-03 09:06:02 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+    priority_queue<int> pq;
+    while (n--) {
+        int x;
+        cin >> x;
+        if (!x) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+}
