309bcf6a72d222948f97441e24e0f6c864ec9aa5
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-03 09:42:47 +0900

    [Silver II] Title: N번째 큰 수, Time: 312 ms, Memory: 11896 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,32 +1,30 @@
 #include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
+using pii = pair<int, int>;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
+    fastio;
+
     int n;
     cin >> n;
-    vector<vector<int>> v(n, vector<int>(n));
+    vector<stack<int>> v(n);
+    priority_queue<pii> pq;
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> v[j][i];
+        for (int j = 0, x; j < n; ++j) {
+            cin >> x;
+            if (i == n-1) {
+                pq.push({x, j});
+            } else {
+                v[j].push(x);
+            }
         }
     }
     for (int i = 0; i < n-1; ++i) {
-        int mmax = INT_MIN;
-        int idx;
-        for (int j = 0; j < n; ++j) {
-            if (mmax < v[j].back()) {
-                mmax = v[j].back();
-                idx = j;
-            }
-        }
-        v[idx].pop_back();
+        int col = pq.top().second;
+        pq.pop();
+        pq.push({v[col].top(), col});
+        v[col].pop();
     }
-    int realMax = INT_MIN;
-    for (int i = 0; i < n; ++i) {
-        realMax = max(realMax, v[i].back());
-    }
-    cout << realMax;
+    cout << pq.top().first;
 }
