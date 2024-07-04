6e0f0e4a609e57fd6cd260942e4579ae2f4a4165
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-04 09:48:25 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,41 +1,12 @@
 #include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
 
-int n;
-int a[14]; // col
-int b[27]; // r-dg
-int c[27]; // l-dg
-int cnt = 0;
-void dfs(int k) {
-    if (k == n) {
-        // cases completed
-        cnt++;
-        return;
-    }
-    
-    // k = 행. 0 ... n-1
-    for (int i = 0; i < n; ++i) {
-        if (a[i]) continue;
-        // k행, i열.
-        if (b[k-i+n-1]) continue;
-        // r-dg
-        if (c[k+i]) continue;
-        // l-dg
-        a[i] = 1;
-        b[k-i+n-1] = 1;
-        c[k+i] = 1;
-        dfs(k+1);
-        a[i] = 0;
-        b[k-i+n-1] = 0;
-        c[k+i] = 0;
-    }
-}
-
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
+    fastio;
+
+    int n;
     cin >> n;
-    dfs(0);
-    cout << cnt;
+    vector<int> ans = {1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184};
+    cout << ans[n-1];
 }
