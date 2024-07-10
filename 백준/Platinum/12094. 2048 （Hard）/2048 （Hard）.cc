c7740c083fa242ad64f391ae861e06ff657f7da4
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-10 17:40:25 +0900

    [Platinum IV] Title: 2048 (Hard), Time: 596 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Platinum/12094. 2048 （Hard）/2048 （Hard）.cc
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int n;
+int ans = 2;
+const int MAX = 10;
+int memo[MAX+1];
+void move(vector<vector<int>>& board, int k) {
+//    cout << k << '\n';
+//    for (int i = 1; i <= n; ++i) {
+//        for (int j = 1; j <= n; ++j) {
+//            cout << board[i][j] << ' ';
+//        }
+//        cout << '\n';
+//    }
+//    cout << '\n';
+
+    int tmpMax = 2;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            tmpMax = max(tmpMax, board[i][j]);
+        }
+    }
+    if (k == MAX) {
+        if (tmpMax > ans) {
+            ans = tmpMax;
+            for (int i = MAX; i >= 0; --i) {
+                memo[i] = tmpMax;
+                tmpMax >>= 1;
+            }
+        }
+        return;
+    }
+    if (tmpMax <= memo[k]) return;
+    vector<vector<int>> newBoard(n+1, vector<int>(n+1));
+
+    // down
+    for (int i = 1; i <= n; ++i) {
+        int prev = -1, cur = -1, idx = n;
+        for (int j = n; j >= 1; --j) {
+            if (!board[j][i]) continue;
+            if (prev == -1) prev = board[j][i];
+            else {
+                cur = board[j][i];
+                if (cur == prev) {
+                    newBoard[idx--][i] = (prev << 1);
+                    prev = -1;
+                } else {
+                    newBoard[idx--][i] = prev;
+                    prev = cur;
+                }
+            }
+        }
+        if (prev != -1) newBoard[idx--][i] = prev;
+//        for (int j = idx; j >= 1; --j) newBoard[j][i] = 0;
+    }
+    if (board != newBoard) move(newBoard, k+1);
+
+    // up
+    for (int i = 1; i <= n; ++i) {
+        int prev = -1, cur = -1, idx = 1;
+        for (int j = 1; j <= n; ++j) {
+            if (!board[j][i]) continue;
+            if (prev == -1) prev = board[j][i];
+            else {
+                cur = board[j][i];
+                if (cur == prev) {
+                    newBoard[idx++][i] = (prev << 1);
+                    prev = -1;
+                } else {
+                    newBoard[idx++][i] = prev;
+                    prev = cur;
+                }
+            }
+        }
+        if (prev != -1) newBoard[idx++][i] = prev;
+        for (int j = idx; j <= n; ++j) newBoard[j][i] = 0;
+    }
+    if (board != newBoard) move(newBoard, k+1);
+
+    // right
+    for (int i = 1; i <= n; ++i) {
+        int prev = -1, cur = -1, idx = n;
+        for (int j = n; j >= 1; --j) {
+            if (!board[i][j]) continue;
+            if (prev == -1) prev = board[i][j];
+            else {
+                cur = board[i][j];
+                if (cur == prev) {
+                    newBoard[i][idx--] = (prev << 1);
+                    prev = -1;
+                } else {
+                    newBoard[i][idx--] = prev;
+                    prev = cur;
+                }
+            }
+        }
+        if (prev != -1) newBoard[i][idx--] = prev;
+        for (int j = idx; j >= 1; --j) newBoard[i][j] = 0;
+    }
+    if (board != newBoard) move(newBoard, k+1);
+
+    // left
+    for (int i = 1; i <= n; ++i) {
+        int prev = -1, cur = -1, idx = 1;
+        for (int j = 1; j <= n; ++j) {
+            if (!board[i][j]) continue;
+            if (prev == -1) prev = board[i][j];
+            else {
+                cur = board[i][j];
+                if (cur == prev) {
+                    newBoard[i][idx++] = (prev << 1);
+                    prev = -1;
+                } else {
+                    newBoard[i][idx++] = prev;
+                    prev = cur;
+                }
+            }
+        }
+        if (prev != -1) newBoard[i][idx++] = prev;
+        for (int j = idx; j <= n; ++j) newBoard[i][j] = 0;
+    }
+    if (board != newBoard) move(newBoard, k+1);
+}
+
+int main() {
+    fastio;
+
+    cin >> n;
+    vector<vector<int>> board(n+1, vector<int>(n+1));
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin >> board[i][j];
+            ans = max(ans, board[i][j]);
+        }
+    }
+    move(board, 0);
+    cout << ans;
+}
