a7a2e413d220fc13a55ba0b30ad4d10d95cec451
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-13 01:46:21 +0900

    [Gold I] Title: 구간 합 구하기, Time: 212 ms, Memory: 41088 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,49 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-long long sum(vector<long long> &tree, int i) {
-    long long ans = 0;
-    while (i > 0) {
-        ans += tree[i];
-        i -= (i & -i);
+void init(vector<ll>& v, vector<ll>& tree, int node, int start, int end) {
+    if (start == end) {
+        tree[node] = v[start];
+        return;
     }
-    return ans;
+    init(v, tree, node*2, start, (start+end)/2);
+    init(v, tree, node*2+1, (start+end)/2+1, end);
+    tree[node] = tree[node*2] + tree[node*2+1];
 }
-void update(vector<long long> &tree, int i, long long diff) {
-    while (i < tree.size()) {
-        tree[i] += diff;
-        i += (i & -i);
+
+void update(vector<ll>& v, vector<ll>& tree, int node, int start, int end, int index, ll val) {
+    if (index < start || index > end) return;
+    if (start == end) {
+        v[index] = val;
+        tree[node] = val;
+        return;
     }
+    update(v, tree, node*2, start, (start+end)/2, index, val);
+    update(v, tree, node*2+1, (start+end)/2+1, end, index, val);
+    tree[node] = tree[node*2] + tree[node*2+1];
 }
+
+ll query(vector<ll>& tree, int node, int start, int end, int left, int right) {
+    if (left <= start && end <= right) return tree[node];
+    if (end < left || start > right) return 0;
+    ll lSum = query(tree, node*2, start, (start+end)/2, left, right);
+    ll rSum = query(tree, node*2+1, (start+end)/2+1, end, left, right);
+    return lSum + rSum;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int n, m, k;
     cin >> n >> m >> k;
-    
-    vector<long long> v(n+1);
-    vector<long long> tree(n+1);
-    
-    for (int i=1; i<=n; i++) {
-        cin >> v[i];
-        update(tree, i, v[i]);
-    }
+    vector<ll> v(n+1);
+    vector<ll> tree(n*4);
+    for (int i = 1; i <= n; ++i) cin >> v[i];
+    init(v, tree, 1, 1, n);
     m += k;
     while (m--) {
         int a;
         cin >> a;
         if (a == 1) {
             int b;
-            long long c;
+            ll c;
             cin >> b >> c;
-            long long diff = c-v[b];
-            v[b] = c;
-            update(tree, b, diff);
+            update(v, tree, 1, 1, n, b, c);
         } else {
-            int b,c;
+            int b, c;
             cin >> b >> c;
-            cout << sum(tree, c) - sum(tree, b-1) << '\n';
+            cout << query(tree, 1, 1, n, b, c) << '\n';
         }
     }
 }
