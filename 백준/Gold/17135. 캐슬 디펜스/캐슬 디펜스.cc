a871da58f9ad6dcda179d76ade154c1956ddaf53
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-12 20:04:58 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 16 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.cc
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.cc
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
 using pii = pair<int, int>;
@@ -8,21 +7,16 @@ int n, m, d;
 vector<vector<int>> board;
 int dx[] = {0, -1, 0};
 int dy[] = {-1, 0, 1};
-
 int bfs(vector<int>& cols) {
     vector<vector<int>> tmpBoard = board;
     int cnt = 0;
     for (int i = n; i >= 1; --i) {
         set<pii> st;
         for (int j = 0; j < 3; ++j) {
-            // i, cols[j]
             queue<pii> Q;
             Q.push({i, cols[j]});
-            vector<vector<int>> dist(n+1, vector<int>(m+1)); // 0 : 미방문
+            vector<vector<int>> dist(n+1, vector<int>(m+1));
             dist[i][cols[j]] = 1;
-            // 왼쪽, 위, 오른쪽순으로 bfs
-            // 탐색 중 1이 발견되면 set에 삽입하고 중단,
-            // 거리가 d를 넘어가면 탐색을 중단한다.
             while (Q.size()) {
                 int nowX = Q.front().first;
                 int nowY = Q.front().second;
@@ -31,7 +25,7 @@ int bfs(vector<int>& cols) {
                     st.insert({nowX, nowY});
                     break;
                 }
-                if (dist[nowX][nowY] == d) continue; // 거리가 d가 될 때 더이상 다음으로 넘어가지 않음
+                if (dist[nowX][nowY] == d) continue;
                 for (int k = 0; k < 3; ++k) {
                     int nextX = nowX + dx[k];
                     int nextY = nowY + dy[k];
@@ -43,17 +37,14 @@ int bfs(vector<int>& cols) {
             }
         }
         cnt += st.size();
-        for (auto& [a, b] : st) {
-            tmpBoard[a][b] = 0;
-        }
+        for (auto& [a, b] : st) tmpBoard[a][b] = 0;
     }
     return cnt;
 }
 
 int main() {
     fastio;
-
-
+    
     cin >> n >> m >> d;
     board.resize(n+1, vector<int>(m+1));
     for (int i = 1; i <= n; ++i) {
@@ -61,7 +52,6 @@ int main() {
             cin >> board[i][j];
         }
     }
-    // m개의 열 중 3개 고르기
     int ans = 0;
     for (int i = 1; i <= m-2; ++i) {
         for (int j = i+1; j <= m-1; ++j) {
