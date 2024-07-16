630a9040f0024eab38036eebc252baf64e7a2ec9
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-17 02:47:06 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 68 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.cc
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.cc
@@ -36,40 +36,34 @@ int main() {
             int tmp = i;
             vector<int> rotates(5);
             for (int j = 0; j < 5; ++j, tmp >>= 2) rotates[j] = tmp & 3;
-            for (int j = 0; j < (1 << 2); ++j) {
-                Point s = {0, 0, 0};
-                Point e = {0, 0, 4};
-                if (j & 1) s.x = 4;
-                else e.x = 4;
-                if (j & 2) s.y = 4;
-                else e.y = 4;
-                if (!cube[rotates[s.x]][order[s.x]][s.y][s.z] || !cube[rotates[e.x]][order[e.x]][e.y][e.z]) continue;
-                queue<Point> Q;
-                Q.push(s);
-                vector<vector<vector<int>>> dist(5, vector<vector<int>>(5, vector<int>(5, -1)));
-                dist[s.x][s.y][s.z] = 0;
-                while (Q.size()) {
-                    Point now = Q.front();
-                    Q.pop();
-                    if (now.x == e.x && now.y == e.y && now.z == e.z) {
-                        ans = min(ans, dist[e.x][e.y][e.z]);
-                        if (ans == 12) {
-                            cout << ans;
-                            return 0;
-                        }
-                        break;
+            Point s = {0, 0, 0};
+            Point e = {4, 4, 4};
+            if (!cube[rotates[s.x]][order[s.x]][s.y][s.z] || !cube[rotates[e.x]][order[e.x]][e.y][e.z]) continue;
+            queue<Point> Q;
+            Q.push(s);
+            vector<vector<vector<int>>> dist(5, vector<vector<int>>(5, vector<int>(5, -1)));
+            dist[s.x][s.y][s.z] = 0;
+            while (Q.size()) {
+                Point now = Q.front();
+                Q.pop();
+                if (now.x == e.x && now.y == e.y && now.z == e.z) {
+                    ans = min(ans, dist[e.x][e.y][e.z]);
+                    if (ans == 12) {
+                        cout << ans;
+                        return 0;
                     }
-                    for (int k = 0; k < 6; ++k) {
-                        Point next;
-                        next.x = now.x + dx[k];
-                        next.y = now.y + dy[k];
-                        next.z = now.z + dz[k];
-                        if (next.x < 0 || next.x > 4 || next.y < 0 || next.y > 4 || next.z < 0 || next.z > 4) continue;
-                        if (!cube[rotates[next.x]][order[next.x]][next.y][next.z]) continue;
-                        if (dist[next.x][next.y][next.z] != -1) continue;
-                        dist[next.x][next.y][next.z] = dist[now.x][now.y][now.z] + 1;
-                        Q.push(next);
-                    }
+                    break;
+                }
+                for (int k = 0; k < 6; ++k) {
+                    Point next;
+                    next.x = now.x + dx[k];
+                    next.y = now.y + dy[k];
+                    next.z = now.z + dz[k];
+                    if (next.x < 0 || next.x > 4 || next.y < 0 || next.y > 4 || next.z < 0 || next.z > 4) continue;
+                    if (!cube[rotates[next.x]][order[next.x]][next.y][next.z]) continue;
+                    if (dist[next.x][next.y][next.z] != -1) continue;
+                    dist[next.x][next.y][next.z] = dist[now.x][now.y][now.z] + 1;
+                    Q.push(next);
                 }
             }
         }
