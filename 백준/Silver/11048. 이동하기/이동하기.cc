a6b9e1a71d5caad83fe5c6efc3e08f07aea9c788
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-16 23:30:11 +0900

    [Silver II] Title: 이동하기, Time: 64 ms, Memory: 9956 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.cc
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -1,46 +1,23 @@
 #include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
-int board[1001][1001];
-int cache[1001][1001];
-int n, m;
-int dp(int x, int y) {
-    // x, y 는 좌표.
-    // 경로의 최댓값을 리턴한다.
-    int mmax = 0;
-    if (x+1 <= n) {
-        if (cache[x+1][y] == -1) {
-            cache[x+1][y] = dp(x+1, y);
-        }
-        mmax = max(mmax, cache[x+1][y]);
-    }
-    if (y+1 <= m) {
-        if (cache[x][y+1] == -1) {
-            cache[x][y+1] = dp(x, y+1);
-        }
-        mmax = max(mmax, cache[x][y+1]);
-    }
-    if (x+1 <= n && y+1 <= m) {
-        if (cache[x+1][y+1] == -1) {
-            cache[x+1][y+1] = dp(x+1, y+1);
-        }
-        mmax = max(mmax, cache[x+1][y+1]);
-    }
-    
-    return board[x][y] + mmax;
-}
+
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    fastio;
+
+    int n, m;
     cin >> n >> m;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            cache[i][j] = -1;
-        }
-    }
+    vector<vector<int>> board(n+1, vector<int>(m+1));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
             cin >> board[i][j];
         }
     }
-    cout << dp(1, 1);
+    vector<vector<int>> dp(n+1, vector<int>(m+1));
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            dp[i][j] = max({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + board[i][j];
+        }
+    }
+    cout << dp[n][m];
 }
