53c2c80200c3bb51636e95348f41b8ce17d2f85a
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-22 12:12:30 +0900

    [Gold V] Title: 사다리 타기, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/2469. 사다리 타기/사다리 타기.cc
+++ 백준/Gold/2469. 사다리 타기/사다리 타기.cc
@@ -4,10 +4,7 @@ using namespace std;
 
 int main() {
     fastio;
-
-    // 감추어진 가로 줄까지 각각 위, 아래에서 탐색 진행.
-    // 감추어진 가로 줄 바로 위, 바로 아래에 배치된 알파벳에 따라
-    // * or -를 결정.
+    
     int k, n;
     cin >> k >> n;
     vector<int> top(k), bottom(k);
@@ -23,8 +20,6 @@ int main() {
         cin >> ladder[i];
         if (ladder[i][0] == '?') hiddenLine = i;
     }
-    // 0...i-1
-    // i+1...n-1
     for (int i = 0; i < hiddenLine; ++i) {
         for (int j = 0; j < k; ++j) {
             if (top[j] == 0) {
@@ -55,46 +50,18 @@ int main() {
             }
         }
     }
-
-//    for (int i = 0; i < k; ++i) {
-//        cout << top[i] << ' ' << i << '\n';
-//    }
-//    cout << '\n';
-//    vector<int> top2(k);
-//    for (int i = 0; i < k; ++i) {
-//        top2[top[i]] = i;
-//    }
-//
-//    for (int i = 0; i < k; ++i) {
-//        cout << i << ' ' << top2[i] << '\n';
-//    }
-
     vector<int> top2(k), bottom2(k);
     for (int i = 0; i < k; ++i) {
-//         알파벳 : 인덱스 -> 인덱스 : 알파벳으로 전환
         top2[top[i]] = i;
         bottom2[bottom[i]] = i;
     }
-
-//    for (int i = 0; i < k; ++i) {
-//        cout << top2[i] << ' ';
-//    }
-//    cout << '\n';
-//    for (int i = 0; i < k; ++i) {
-//        cout << bottom2[i] << ' ';
-//    }
-//    cout << '\n';
-
-
     string ans;
     bool flag = true;
     for (int i = 0; i < k-1; ++i) {
-        if (top2[i] == bottom2[i]) {
-            ans += '*';
-        } else if (top2[i] == bottom2[i+1] && top2[i+1] == bottom2[i]) {
-            if (i == k-2) {
-                ans += '-';
-            } else {
+        if (top2[i] == bottom2[i]) ans += '*';
+        else if (top2[i] == bottom2[i+1] && top2[i+1] == bottom2[i]) {
+            if (i == k-2) ans += '-';
+            else {
                 ans += "-*";
                 ++i;
             }
@@ -104,24 +71,5 @@ int main() {
         }
     }
     if (flag) cout << ans;
-    else {
-        for (int i = 0; i < k-1; ++i) cout << 'x';
-    }
-//    int cnt = 0;
-//    for (int i = 0; i < k-1; ++i) {
-//        if (top[i] == bottom[i+1] && top[i+1] == bottom[i]) {
-//            ans += "--";
-//            ++i;
-//        }
-//        else if (top[i] == bottom[i] && top[i+1] == bottom[i+1]) {
-//            ans += "**";
-//            ++i;
-//        }
-//        else {
-//            cnt++;
-//        }
-//    }
-//    if (cnt > 1) {
-//        for (int i = 0; i < k-1; ++i) cout << 'x';
-//    } else cout << ans;
+    else for (int i = 0; i < k-1; ++i) cout << 'x';
 }
