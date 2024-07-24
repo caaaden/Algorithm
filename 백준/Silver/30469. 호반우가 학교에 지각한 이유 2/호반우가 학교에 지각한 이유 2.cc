c38135d60300745bf91a053d73194ef1057e39e8
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-24 09:21:24 +0900

    [Silver I] Title: 호반우가 학교에 지각한 이유 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/30469. 호반우가 학교에 지각한 이유 2/호반우가 학교에 지각한 이유 2.cc
+++ 백준/Silver/30469. 호반우가 학교에 지각한 이유 2/호반우가 학교에 지각한 이유 2.cc
@@ -5,21 +5,8 @@ using namespace std;
 int main() {
     fastio;
 
-    // 1 -> 1
-    // 3 -> 1
-    // 7 -> 1
-    // 9 -> 7 -> 1
-
-    // 1 -> ... -> 1
-
-    // 1 -> 1
-    // 1 -> 3
-    // 1 -> 7
-    // 1 -> 9
-
     int a, b, n;
     cin >> a >> b >> n;
-    // b의 십의 자리 수가 1, 3, 7, 9가 아니면 -1
     for (auto& e : {2, 4, 5, 6, 8}) {
         if (b / 10 == e) {
             cout << -1;
@@ -30,9 +17,7 @@ int main() {
     if (a % 10 == 9) {
         cout << 7;
         for (int i = 0; i < n-5; ++i) cout << 1;
-    } else {
-        for (int i = 0; i < n-4; ++i) cout << 1;
-    }
+    } else for (int i = 0; i < n-4; ++i) cout << 1;
     cout << b;
 
 }
