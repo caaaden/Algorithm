21e7be8f2a2bee15e802a7ba76355a7be5ef3d23
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-25 08:40:29 +0900

    [Gold IV] Title: 개근상, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/1563. 개근상/개근상.cc
+++ 백준/Gold/1563. 개근상/개근상.cc
@@ -15,40 +15,16 @@ int main() {
     for (int i = 1; i < n; ++i) {
         for (int j = 0; j < 2; ++j) {
             for (int k = 0; k < 3; ++k) {
-                dp[i+1][j][0] += dp[i][j][k];
-                if (j == 0) dp[i+1][j+1][0] += dp[i][j][k];
-                if (k <= 1) dp[i+1][j][k+1] += dp[i][j][k];
-            }
-        }
-        
-//        dp[i+1][0][0] += dp[i][0][0];
-//        dp[i+1][1][0] += dp[i][0][0];
-//        dp[i+1][0][1] += dp[i][0][0];
-//
-//        dp[i+1][0][0] += dp[i][0][1];
-//        dp[i+1][1][0] += dp[i][0][1];
-//        dp[i+1][0][2] += dp[i][0][1];
-//
-//        dp[i+1][0][0] += dp[i][0][2];
-//        dp[i+1][1][0] += dp[i][0][2];
-//
-//        dp[i+1][1][0] += dp[i][1][0];
-//        dp[i+1][1][1] += dp[i][1][0];
-//
-//        dp[i+1][1][0] += dp[i][1][1];
-//        dp[i+1][1][2] += dp[i][1][1];
-//
-//        dp[i+1][1][0] += dp[i][1][2];
-        for (int j = 0; j < 2; ++j) {
-            for (int k = 0; k < 3; ++k) {
-                dp[i+1][j][k] %= MOD;
+                dp[i+1][j][0] += dp[i][j][k] % MOD;
+                if (!j) dp[i+1][j+1][0] += dp[i][j][k] % MOD;
+                if (k <= 1) dp[i+1][j][k+1] += dp[i][j][k] % MOD;
             }
         }
     }
     int ans = 0;
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 3; ++j) {
-            ans += dp[n][i][j];
+            ans += dp[n][i][j] % MOD;
         }
     }
     ans %= MOD;
