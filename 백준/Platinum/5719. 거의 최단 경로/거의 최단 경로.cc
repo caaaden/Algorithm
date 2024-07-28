9c5a285685425f8e18fc38df69a4a5c1e099056b
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-28 13:49:10 +0900

    [Platinum V] Title: 거의 최단 경로, Time: 44 ms, Memory: 3288 KB -BaekjoonHub

---
+++ 백준/Platinum/5719. 거의 최단 경로/거의 최단 경로.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+using pii = pair<int, int>;
+
+int n, m;
+int s, d;
+vector<vector<pii>> graph;
+vector<vector<int>> path;
+vector<vector<int>> check;
+const int MAX = 1e9;
+int dijkstra() {
+    vector<int> dist(n, MAX);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    dist[s] = 0;
+    pq.push({0, s});
+    while (pq.size()) {
+        int cost = pq.top().first;
+        int now = pq.top().second;
+        pq.pop();
+        if (dist[now] < cost) continue;
+        for (auto& [next, nextCost] : graph[now]) {
+            if (check[now][next]) continue;
+            if (cost + nextCost < dist[next]) {
+                path[next].clear();
+                path[next].push_back(now);
+                dist[next] = cost + nextCost; // s -> now, now -> next
+                pq.push({dist[next], next});
+            } else if (cost + nextCost == dist[next]) {
+                path[next].push_back(now);
+            }
+        }
+    }
+    return dist[d];
+}
+
+int main() {
+    fastio;
+
+    while (true) {
+        cin >> n >> m;
+        if (!n && !m) break;
+        graph.clear();
+        graph.resize(n);
+        path.clear();
+        path.resize(n);
+        check.clear();
+        check.resize(n, vector<int>(n));
+        vector<int> vis(n); // 경로 역추적 방문 배열
+        cin >> s >> d; // start, destination
+        for (int i = 0; i < m; ++i) {
+            int u, v, p;
+            cin >> u >> v >> p;
+            graph[u].push_back({v, p});
+        }
+        // 최단경로 찾기, 최단경로 역추적
+        dijkstra(); // path 갱신
+
+        vis[d] = 1;
+        queue<int> Q;
+        Q.push(d);
+        while (Q.size()) {
+            int now = Q.front();
+            Q.pop();
+            if (now == s) continue;
+            for (auto& next : path[now]) {
+                check[next][now] = 1;
+                if (vis[next]) continue;
+                vis[next] = 1;
+                Q.push(next);
+            }
+        }
+        // 다익스트라를 진행하되 check = 1이면 continue
+        int ans = dijkstra();
+        cout << (ans == MAX ? -1 : ans) << '\n';
+    }
+}
