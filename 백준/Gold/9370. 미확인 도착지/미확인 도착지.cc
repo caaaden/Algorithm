03253d4d66059ba6423af3eb20fbd2599fe20a5c
caaaden <121105930+caaaden@users.noreply.github.com>
2024-07-30 20:59:01 +0900

    [Gold II] Title: 미확인 도착지, Time: 40 ms, Memory: 3508 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+using pii = pair<int, int>;
+using piii = pair<int, pii>;
+
+const int MAX = 1e9;
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, t;
+        cin >> n >> m >> t;
+        int s, g, h;
+        cin >> s >> g >> h;
+        vector<vector<pii>> graph(n+1);
+        int gh; // g-h 거리
+        for (int i = 0; i < m; ++i) {
+            int a, b, d;
+            cin >> a >> b >> d;
+            if (a == g && b == h || a == h && b == g) gh = d;
+            graph[a].push_back({b, d});
+            graph[b].push_back({a, d});
+        }
+        vector<int> dest(t);
+        for (auto& e : dest) cin >> e;
+        auto dijkstra = [&](int start) {
+            vector<int> dist(n+1, MAX);
+            priority_queue<pii, vector<pii>, greater<pii>> pq;
+            dist[start] = 0;
+            pq.push({0, start});
+            while (pq.size()) {
+                int cost = pq.top().first;
+                int now = pq.top().second;
+                pq.pop();
+                if (dist[now] < cost) continue;
+                for (auto& [next, nowToNxt] : graph[now]) {
+                    int nextCost = cost + nowToNxt;
+                    if (nextCost < dist[next]) {
+                        dist[next] = nextCost;
+                        pq.push({nextCost, next});
+                    }
+                }
+            }
+            return dist;
+        };
+        vector<int> dist1 = dijkstra(s);
+        vector<int> dist2 = dijkstra(g);
+        vector<int> dist3 = dijkstra(h);
+        // dist1[g] + gh + dist3[?]
+        // dist1[h] + gh + dist2[?]
+        // dist1[?]
+        vector<int> ans;
+        for (auto& e : dest) {
+            if (dist1[g] + gh + dist3[e] == dist1[e] || dist1[h] + gh + dist2[e] == dist1[e]) {
+                ans.push_back(e);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        for (auto& e : ans) cout << e << ' ';
+        cout << '\n';
+    }
+}
