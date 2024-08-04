dde9843b2ee6b52d10a7ceb7b39831b82b5d2cef
caaaden <121105930+caaaden@users.noreply.github.com>
2024-08-04 11:12:33 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 6092 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -5,21 +5,17 @@ using namespace std;
 int main() {
     fastio;
 
-    int n, m; // 앱의 수, 필요한 메모리
+    int n, m;
     cin >> n >> m;
     vector<int> v(n+1);
     for (int i = 1; i <= n; ++i) cin >> v[i];
-    vector<int> c(n+1); // cost
+    vector<int> c(n+1);
     for (int i = 1; i <= n; ++i) cin >> c[i];
-    // 101, 10001
     vector<vector<int>> dp(n+1, vector<int>(10001));
     for (int i = 1; i <= n; ++i) {
         for (int j = 0; j <= 10000; ++j) {
-            // 각 앱의 모든 비용에 대하여,
-            // c[i]
-            // j >= c[i]
+            dp[i][j] = dp[i-1][j];
             if (j >= c[i]) dp[i][j] = max(dp[i-1][j], dp[i-1][j-c[i]]+v[i]);
-            else dp[i][j] = dp[i-1][j]; //
             if (i == n) {
                 if (dp[i][j] >= m) {
                     cout << j;
