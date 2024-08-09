a607e6db8923c0649bd4e90601cd07dc83eea6fa
caaaden <121105930+caaaden@users.noreply.github.com>
2024-08-09 18:39:20 +0900

    [Silver I] Title: 완전 P제곱수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4320. 완전 P제곱수/완전 P제곱수.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+using i64 = long long;
+
+int main() {
+    fastio;
+
+    while (true) {
+        i64 n;
+        cin >> n;
+        if (!n) break;
+        if (n > 0) {
+            int ans = 1;
+            for (i64 i = 2; ans == 1 && i*i <= n; ++i) {
+                i64 pow = i;
+                for (int j = 2; j <= 30; ++j) {
+                    pow *= i;
+                    if (pow > n) break;
+                    if (pow == n) {
+                        ans = j;
+                        break;
+                    }
+                }
+            }
+            cout << ans << '\n';
+        } else {
+            int ans = 1;
+            for (i64 i = -2; ans == 1 && i*i*i >= n; --i) {
+                i64 pow = i;
+                for (int j = 3; j <= 31; j += 2) {
+                    pow *= i*i;
+                    if (pow < n) break;
+                    if (pow == n) {
+                        ans = j;
+                        break;
+                    }
+                }
+            }
+            cout << ans << '\n';
+        }
+    }
+}
