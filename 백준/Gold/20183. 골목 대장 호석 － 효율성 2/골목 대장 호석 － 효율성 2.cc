5e657d4f50518b30c6da6a6c5925472b386093af
caaaden <121105930+caaaden@users.noreply.github.com>
2024-08-17 16:57:04 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1588 ms, Memory: 23664 KB, Score: 42 point -BaekjoonHub

---
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+using i64 = long long;
+using pil = pair<int, i64>;
+using pii = pair<int, int>;
+using pli = pair<i64, int>;
+
+const i64 MAX = 1e18;
+int main() {
+    fastio;
+
+    int n, m, a, b;
+    i64 c;
+    cin >> n >> m >> a >> b >> c;
+    vector<vector<pii>> graph(n+1);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+        graph[v].push_back({u, w});
+    }
+    // 골목 최대 수금량 제한을 이분탐색으로 찾기
+
+    // 골목 최대 수금량 제한을 가진 다익스트라
+    auto dijkstra = [&](int mid) {
+        // 간선의 가중치가 mid 초과이면 중단
+        vector<i64> dist(n+1, MAX);
+        priority_queue<pli, vector<pli>, greater<pli>> pq;
+        dist[a] = 0;
+        pq.push({0, a});
+        while (pq.size()) {
+            i64 cost = pq.top().first;
+            int now = pq.top().second;
+            pq.pop();
+            if (dist[now] < cost) continue;
+            for (auto& [next, nowToNxt] : graph[now]) {
+                if (nowToNxt > mid) continue;
+                i64 nextCost = cost + nowToNxt;
+                if (nextCost > c) continue;
+                if (nextCost < dist[next]) {
+                    dist[next] = nextCost;
+                    pq.push({nextCost, next});
+                }
+            }
+        }
+        return dist[b] == MAX ? false : true;
+    };
+    int lo = 0, hi = 1e9;
+    while (lo + 1 < hi) {
+        // FF...FT...TT
+        // 가장 작은 T
+        int mid = (lo + hi) / 2;
+        if (dijkstra(mid)) hi = mid;
+        else lo = mid;
+    }
+    cout << (hi == 1e9 ? -1 : hi);
+}
