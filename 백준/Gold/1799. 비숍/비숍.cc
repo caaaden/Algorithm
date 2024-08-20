44b4e368bb12a1a39faef3030a23f076fcca4c04
caaaden <121105930+caaaden@users.noreply.github.com>
2024-08-20 19:36:23 +0900

    [Gold I] Title: 비숍, Time: 44 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.cc
+++ 백준/Gold/1799. 비숍/비숍.cc
@@ -13,46 +13,46 @@ int main() {
             cin >> board[i][j];
         }
     }
-    // 흑, 백 지역으로 나눠 개수를 각각 구하고 곱해서 출력
-    // 대각선 인덱스 관리
-    // n, n-1
-    //
-    int white = 0, black = 0; // 구역별 놓을 수 있는 최대 비숍 개수
-    vector<int> diag1(2*n-1); // 왼쪽아래방향 대각선
-    unordered_map<int, int> diag2; // 인덱스 음수 계산
-    function<void(int, int)> dfs = [&](int k, int idx) {
-        white = max(white, k);
-        for (int i = idx; i < n*n; ++i) {
-            int a = i / n;
-            int b = i % n;
-            if ((a+b) & 1) continue;
-            if (!board[a][b]) continue;
-            if (diag1[a+b]) continue;
-            if (diag2[a-b]) continue;
-            diag1[a+b] = 1;
-            diag2[a-b] = 1;
-            dfs(k+1, i+1);
-            diag1[a+b] = 0;
-            diag2[a-b] = 0;
+    int white = 0, black = 0;
+//    vector<int> diag1(2*n-1);
+    unordered_map<int, int> diag2;
+    function<void(int, int)> dfs = [&](int k, int depth) {
+        // k : diag1의 인덱스
+        // depth : dfs의 깊이, 놓은 비숍의 개수
+        white = max(white, depth);
+        for (int i = k; i < 2*n-1; ++i) {
+            if (i & 1) continue;
+            for (int j = 0; j <= i; ++j) {
+                int x = j;
+                int y = i-j;
+                if (x > n-1 || y > n-1) continue;
+                if (!board[x][y]) continue;
+                if (diag2[x-y]) continue;
+                diag2[x-y] = 1;
+                dfs(i+1, depth+1);
+                diag2[x-y] = 0;
+            }
         }
     };
-    function<void(int, int)> dfs2 = [&](int k, int idx) {
-        black = max(black, k);
-        for (int i = idx; i < n*n; ++i) {
-            int a = i / n;
-            int b = i % n;
-            if ((a+b) % 2 == 0) continue;
-            if (!board[a][b]) continue;
-            if (diag1[a+b]) continue;
-            if (diag2[a-b]) continue;
-            diag1[a+b] = 1;
-            diag2[a-b] = 1;
-            dfs2(k+1, i+1);
-            diag1[a+b] = 0;
-            diag2[a-b] = 0;
+    function<void(int, int)> dfs2 = [&](int k, int depth) {
+        // k : diag1의 인덱스
+        // depth : dfs의 깊이, 놓은 비숍의 개수
+        black = max(black, depth);
+        for (int i = k; i < 2*n-1; ++i) {
+            if (!(i & 1)) continue;
+            for (int j = 0; j <= i; ++j) {
+                int x = j;
+                int y = i-j;
+                if (x > n-1 || y > n-1) continue;
+                if (!board[x][y]) continue;
+                if (diag2[x-y]) continue;
+                diag2[x-y] = 1;
+                dfs2(i+1, depth+1);
+                diag2[x-y] = 0;
+            }
         }
     };
     dfs(0, 0);
     dfs2(0, 0);
-    cout << white + black;
+    cout << white+black;
 }
