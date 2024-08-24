f3189c7de49e4f7b56857726650702c6bfae0602
caaaden <121105930+caaaden@users.noreply.github.com>
2024-08-25 08:06:28 +0900

    [Silver I] Title: 자릿수, Time: 0 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Silver/28293. 자릿수/자릿수.cc
+++ 백준/Silver/28293. 자릿수/자릿수.cc
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
-using i64 = long long;
-using pii = pair<int, int>;
 
 int main() {
     fastio;
 
     int a, b;
     cin >> a >> b;
-    // log10(a) * b
     cout << (int)(log10(a)*b)+1;
 }
