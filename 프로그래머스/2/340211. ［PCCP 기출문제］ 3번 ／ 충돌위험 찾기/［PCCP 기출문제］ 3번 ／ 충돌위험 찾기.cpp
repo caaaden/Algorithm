bcedbb6c293b388f9c08b9432e9fbf9b49df1c99
caaaden <121105930+caaaden@users.noreply.github.com>
2024-09-27 20:50:40 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int solution(vector<vector<int>> points, vector<vector<int>> routes) {
-    // check[a][b][c] = (a, b)에 c시간에 존재하는 로봇의 개수
     vector<vector<vector<int>>> check(101, vector<vector<int>>(101, vector<int>(20000)));
     int n = points.size();
     int m = routes[0].size();
@@ -10,20 +9,9 @@ int solution(vector<vector<int>> points, vector<vector<int>> routes) {
     for (auto& route : routes) {
         int t = 0;
         for (int i = 0; i < m-1; ++i) {
-            // route[i], route[i+1]
             int a = route[i], b = route[i+1];
-            // points[a-1] -> points[b-1]
-            // (4, 7) -> (1, 4)
-            // 4 7
-            // 3 7
-            // 2 7
-            // 1 7
-            // 1 6
-            // 1 5
-            // 1 4
             int x1 = points[a-1][0], y1 = points[a-1][1];
             int x2 = points[b-1][0], y2 = points[b-1][1];
-            // (x1, y1) -> (x2, y2)
             if (i == 0) {
                 if (check[x1][y1][t] < 2) {
                     check[x1][y1][t]++;
