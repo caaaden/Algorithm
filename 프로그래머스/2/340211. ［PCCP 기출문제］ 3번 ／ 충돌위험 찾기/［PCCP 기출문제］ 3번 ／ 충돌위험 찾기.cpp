eced335ffb3eed13bf17569acc50f3eb6065f260
caaaden <121105930+caaaden@users.noreply.github.com>
2024-09-29 19:58:16 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 113.09 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
@@ -1,52 +1,126 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
 using namespace std;
+vector<pair<int,int>> cur_pos(100);
+vector<int> stage(100, 1);
+void move(int &goal_r, int &goal_c, int &cur_r, int &cur_c)
+{
+    if(goal_r < cur_r)
+    {
+        cur_r-=1;
+    }
+    else if(goal_r > cur_r)
+    {
+        cur_r+=1;
+    }
+    else if(goal_c < cur_c)
+    {
+        cur_c-=1;
+    }
+    else if(goal_c > cur_c)
+    {
+        cur_c+=1;
+    }
 
+}
 int solution(vector<vector<int>> points, vector<vector<int>> routes) {
-    vector<vector<vector<int>>> check(101, vector<vector<int>>(101, vector<int>(20000)));
+    int answer = 0;
+    int clock = 0;
+    int arrived_robots = 0;
+    int num_robots = routes.size();
     int n = points.size();
     int m = routes[0].size();
-    int ans = 0;
-    for (auto& route : routes) {
-        int t = 0;
-        for (int i = 0; i < m-1; ++i) {
-            int a = route[i], b = route[i+1];
-            int x1 = points[a-1][0], y1 = points[a-1][1];
-            int x2 = points[b-1][0], y2 = points[b-1][1];
-            if (i == 0) {
-                if (check[x1][y1][t] < 2) {
-                    check[x1][y1][t]++;
-                    if (check[x1][y1][t] == 2) ans++;
-                }
-                t++;    
-            }
-            if (x1 < x2) {
-                for (int j = x1+1; j <= x2; ++j, ++t) {
-                    if (check[j][y1][t] == 2) continue;
-                    check[j][y1][t]++;
-                    if (check[j][y1][t] == 2) ans++;
-                }
-            } else {
-                for (int j = x1-1; j >= x2; --j, ++t) {
-                    if (check[j][y1][t] == 2) continue;
-                    check[j][y1][t]++;
-                    if (check[j][y1][t] == 2) ans++;
-                }
-            }
-            if (y1 < y2) {
-                for (int j = y1+1; j <= y2; ++j, ++t) {
-                    if (check[x2][j][t] == 2) continue;
-                    check[x2][j][t]++;
-                    if (check[x2][j][t] == 2) ans++;
-                }
-            } else {
-                for (int j = y1-1; j >= y2; --j, ++t) {
-                    if (check[x2][j][t] == 2) continue;
-                    check[x2][j][t]++;
-                    if (check[x2][j][t] == 2) ans++;
-                }
-            }
-            
+    map<int, int> conflict_pos1;
+    for(int i = 0;i < num_robots;i++)
+    {
+        int route = routes[i][0] - 1;
+        int cur_r = points[route][0] - 1;
+        int cur_c = points[route][1] - 1;
+        cur_pos[i] = make_pair(cur_r, cur_c);
+        int map_k = 100 * cur_r + cur_c;
+        if(conflict_pos1.find(map_k) == conflict_pos1.end())
+        {
+            conflict_pos1[map_k] = 1;
+        }
+        else
+        {
+            conflict_pos1[map_k]++;
+        }
+        // cout << cur_pos[i].first<< " " << cur_pos[i].second << endl;
+    }
+
+    for(auto k: conflict_pos1)
+    {
+        if (k.second > 1)
+        {
+            answer += 1;
         }
     }
-    return ans;
+
+    while(arrived_robots != num_robots)
+    {
+        map<int, int> conflict_pos;
+        for(int i = 0;i < num_robots;i++)
+        {
+            if(stage[i] == m) continue;
+            int goal_idx = routes[i][stage[i]] - 1;
+            int goal_r = points[goal_idx][0] - 1;
+            int goal_c = points[goal_idx][1] - 1;
+
+            int cur_r = cur_pos[i].first;
+            int cur_c = cur_pos[i].second;
+
+            if(goal_r == cur_r && goal_c == cur_c)
+            {
+                stage[i]++;
+                if(stage[i] == m)
+                {
+                    arrived_robots++;
+                    continue;
+                }
+                goal_idx = routes[i][stage[i]] - 1;
+                goal_r = points[goal_idx][0] - 1;
+                goal_c = points[goal_idx][1] - 1;
+            }
+            // cout << i << " - " << cur_r << " " << cur_c << endl;
+            move(goal_r, goal_c, cur_r, cur_c);
+            // cout << i << " - " << cur_r << " " << cur_c << endl;
+            // cout << endl;
+            cur_pos[i].first = cur_r;
+            cur_pos[i].second = cur_c;
+            int map_k = 100 * cur_r + cur_c;
+            if(conflict_pos.find(map_k) == conflict_pos.end())
+            {
+                conflict_pos[map_k] = 1;
+            }
+            else
+            {
+                conflict_pos[map_k]++;
+            }
+//             for(int j = 0;j < i;j++)
+//             {
+//                 int other_r = cur_pos[j].first;
+//                 int other_c = cur_pos[j].second;
+
+//                 if(cur_r == other_r && cur_c == other_c) answer++;
+//             }
+        }
+        // int conflicts = num_robots - conflict_pos.size();
+        // cout << conflicts << endl;
+        int cur_answer = 0;
+        for(auto k: conflict_pos)
+        {
+            if (k.second > 1)
+            {
+                cur_answer += 1;
+            }
+        }
+        // cout << cur_answer << endl;
+        // if(clock == 2) break;
+        answer += cur_answer;
+        clock++;
+    }
+    return answer;
 }
