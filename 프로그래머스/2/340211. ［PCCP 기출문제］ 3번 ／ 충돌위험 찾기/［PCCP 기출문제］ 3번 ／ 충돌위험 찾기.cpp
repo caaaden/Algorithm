6382d44a7a231960f18d79c0930282affa33a75e
caaaden <121105930+caaaden@users.noreply.github.com>
2024-09-29 19:58:49 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 341.53 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
@@ -1,126 +1,125 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <map>
-using namespace std;
-vector<pair<int,int>> cur_pos(100);
-vector<int> stage(100, 1);
-void move(int &goal_r, int &goal_c, int &cur_r, int &cur_c)
-{
-    if(goal_r < cur_r)
-    {
-        cur_r-=1;
-    }
-    else if(goal_r > cur_r)
-    {
-        cur_r+=1;
-    }
-    else if(goal_c < cur_c)
-    {
-        cur_c-=1;
-    }
-    else if(goal_c > cur_c)
-    {
-        cur_c+=1;
-    }
 
-}
-int solution(vector<vector<int>> points, vector<vector<int>> routes) {
-    int answer = 0;
-    int clock = 0;
-    int arrived_robots = 0;
-    int num_robots = routes.size();
-    int n = points.size();
-    int m = routes[0].size();
-    map<int, int> conflict_pos1;
-    for(int i = 0;i < num_robots;i++)
+using namespace std;
+vector<vector<int>> robots;
+vector<int> found;
+int result = 0;
+int routesize = 0;
+
+bool Update(vector<vector<int>>& points, vector<vector<int>>& routes)
+{
+    for(int i=0; i<robots.size(); i++)
     {
-        int route = routes[i][0] - 1;
-        int cur_r = points[route][0] - 1;
-        int cur_c = points[route][1] - 1;
-        cur_pos[i] = make_pair(cur_r, cur_c);
-        int map_k = 100 * cur_r + cur_c;
-        if(conflict_pos1.find(map_k) == conflict_pos1.end())
+
+        if(found[i] == routesize)
         {
-            conflict_pos1[map_k] = 1;
+            continue;
+        }
+
+        int round = found[i];
+        auto goal = points[routes[i][round]-1];
+
+        if(goal[0] == robots[i][0] && goal[1] == robots[i][1])
+        {
+            round++;
+            found[i] = round;
+            if(round == routesize)
+                continue;
+        }
+
+        goal = points[routes[i][round]-1];
+
+        int x = robots[i][0];
+        int y = robots[i][1];
+
+        if(x != goal[0])
+        {
+            int move;
+            if(goal[0] > x)
+                move = 1;
+            else
+                move = -1;
+            robots[i][0] = x + move;
         }
         else
         {
-            conflict_pos1[map_k]++;
-        }
-        // cout << cur_pos[i].first<< " " << cur_pos[i].second << endl;
-    }
-
-    for(auto k: conflict_pos1)
-    {
-        if (k.second > 1)
-        {
-            answer += 1;
-        }
-    }
-
-    while(arrived_robots != num_robots)
-    {
-        map<int, int> conflict_pos;
-        for(int i = 0;i < num_robots;i++)
-        {
-            if(stage[i] == m) continue;
-            int goal_idx = routes[i][stage[i]] - 1;
-            int goal_r = points[goal_idx][0] - 1;
-            int goal_c = points[goal_idx][1] - 1;
-
-            int cur_r = cur_pos[i].first;
-            int cur_c = cur_pos[i].second;
-
-            if(goal_r == cur_r && goal_c == cur_c)
-            {
-                stage[i]++;
-                if(stage[i] == m)
-                {
-                    arrived_robots++;
-                    continue;
-                }
-                goal_idx = routes[i][stage[i]] - 1;
-                goal_r = points[goal_idx][0] - 1;
-                goal_c = points[goal_idx][1] - 1;
-            }
-            // cout << i << " - " << cur_r << " " << cur_c << endl;
-            move(goal_r, goal_c, cur_r, cur_c);
-            // cout << i << " - " << cur_r << " " << cur_c << endl;
-            // cout << endl;
-            cur_pos[i].first = cur_r;
-            cur_pos[i].second = cur_c;
-            int map_k = 100 * cur_r + cur_c;
-            if(conflict_pos.find(map_k) == conflict_pos.end())
-            {
-                conflict_pos[map_k] = 1;
-            }
+            int move;
+            if(goal[1] > y)
+                move = 1;
             else
-            {
-                conflict_pos[map_k]++;
-            }
-//             for(int j = 0;j < i;j++)
-//             {
-//                 int other_r = cur_pos[j].first;
-//                 int other_c = cur_pos[j].second;
+                move = -1;
 
-//                 if(cur_r == other_r && cur_c == other_c) answer++;
-//             }
+            robots[i][1] = y + move;
         }
-        // int conflicts = num_robots - conflict_pos.size();
-        // cout << conflicts << endl;
-        int cur_answer = 0;
-        for(auto k: conflict_pos)
+    }
+
+    return true;
+}
+
+void Check()
+{
+
+    vector<vector<bool>> visited = vector<vector<bool>>(101, vector<bool>(101, false));
+    for(int i=0; i<robots.size(); i++)
+    {
+        if(found[i] == routesize)
+            continue;
+
+        for(int j=i+1; j<robots.size(); j++)
         {
-            if (k.second > 1)
+            if(found[j] == routesize)
+                continue;
+
+            if(robots[i][0] == robots[j][0] && robots[i][1] == robots[j][1])
             {
-                cur_answer += 1;
+                visited[robots[i][0]][robots[i][1]] = true;
+            }
+
+        }
+    }
+
+    for(int i=1; i<=100; i++)
+    {
+        for(int j=1; j<=100; j++)
+        {
+            if(visited[i][j])
+                result++;
+        }
+    }
+}
+
+int solution(vector<vector<int>> points, vector<vector<int>> routes) {
+    int answer = 0;
+    found =vector<int>(routes.size(), 1);
+    routesize = routes[0].size();
+    for(int i=0; i<routes.size(); i++)
+    {
+        auto route = routes[i];
+        robots.push_back(points[route[0]-1]);
+    }
+
+    Check();
+
+    while(true)
+    {
+
+        Update(points, routes);
+        Check();
+
+        bool find = true;
+        for(int i=0; i<robots.size(); i++)
+        {
+            if(found[i] != routesize)
+            {
+                find = false;
+                break;
             }
         }
-        // cout << cur_answer << endl;
-        // if(clock == 2) break;
-        answer += cur_answer;
-        clock++;
+
+        if(find)
+            break;
     }
+    answer = result;
     return answer;
 }
