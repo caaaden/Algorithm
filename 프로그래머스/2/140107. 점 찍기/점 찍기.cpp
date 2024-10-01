68bc219b150f5cde40cdc7e8b39dd6b7f9fefb7f
caaaden <121105930+caaaden@users.noreply.github.com>
2024-10-01 16:18:24 +0900

    [level 2] Title: 점 찍기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -3,11 +3,9 @@ using namespace std;
 using ll = long long;
 
 long long solution(int k, int d) {
-    // x = i*k
     ll sum = 0;
     for (int i = 0; i*k <= d; ++i) {
         int x = i * k;
-        // d * d - x * x
         int y = floor(sqrt((ll)d*d - (ll)x*x));
         sum += floor(y / k) + 1;
     }
