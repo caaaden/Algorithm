8954a3dd21fa0eb06792b98dbeab3ec4452b7c46
caaaden <121105930+caaaden@users.noreply.github.com>
2024-10-04 10:46:41 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 32.31 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -6,17 +6,9 @@ vector<long long> solution(vector<long long> numbers) {
     vector<ll> ans;
     ans.reserve(numbers.size());
     for (auto& num : numbers) {
-        if (~num & 1) {
-            ans.push_back(num + 1);
-        } else {
-            ll tmp = num;
-            int cnt = 0;
-            while (tmp & 1) {
-                cnt++;
-                tmp >>= 1;
-            }
-            ans.push_back(num + (1LL << cnt) - (1LL << (cnt-1)));
-        }
+        ll pow = 1;
+        while (num & pow) pow <<= 1;
+        ans.push_back(num + pow - (pow >> 1));
     }
     return ans;
 }
