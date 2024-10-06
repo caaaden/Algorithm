42b5e8a7b67b4c07683989f0f9558a78e80f0b1d
caaaden <121105930+caaaden@users.noreply.github.com>
2024-10-06 16:26:59 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.98 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 using pii = pair<int, int>;
 
-pii func(int x, int y, int sz, vector<vector<int>>& board) {
+pii compress(int x, int y, int sz, vector<vector<int>>& board) {
     if (sz == 1) {
         if (board[x][y]) return {0, 1};
         return {1, 0};
     }
     int half = sz / 2;
-    pii a = func(x, y, half, board);
-    pii b = func(x, y+half, half, board);
-    pii c = func(x+half, y, half, board);
-    pii d = func(x+half, y+half, half, board);
+    pii a = compress(x, y, half, board);
+    pii b = compress(x, y+half, half, board);
+    pii c = compress(x+half, y, half, board);
+    pii d = compress(x+half, y+half, half, board);
     pii sum = {a.X+b.X+c.X+d.X, a.Y+b.Y+c.Y+d.Y};
     if (sum.X == 4 && sum.Y == 0) {
         return {1, 0};
@@ -26,6 +26,6 @@ pii func(int x, int y, int sz, vector<vector<int>>& board) {
 
 vector<int> solution(vector<vector<int>> arr) {
     int n = arr.size();
-    pii ans = func(0, 0, n, arr);
+    pii ans = compress(0, 0, n, arr);
     return {ans.first, ans.second};
 }
