6f9bcc44560342b42e53dad4126b55bb42312ca5
caaaden <121105930+caaaden@users.noreply.github.com>
2024-10-09 13:50:57 +0900

    [Gold V] Title: LCS, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -8,12 +8,13 @@ int main() {
     string a, b;
     cin >> a >> b;
     int n = a.size(), m = b.size();
-    vector<vector<int>> dp(n+1, vector<int>(m+1));
+    vector<vector<int>> dp(2, vector<int>(m+1));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
-            if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            if (a[i-1] == b[j-1]) dp[1][j] = dp[0][j-1] + 1;
+            else dp[1][j] = max(dp[0][j], dp[1][j-1]);
         }
+        swap(dp[0], dp[1]);
     }
-    cout << dp[n][m];
+    cout << dp[0][m];
 }
