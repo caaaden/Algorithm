14c775b0836855696ce420d72d525cc45a1b9eeb
caaaden <121105930+caaaden@users.noreply.github.com>
2024-10-09 12:03:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 3200 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,35 +1,41 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define all(v) (v).begin(), (v).end()
+using namespace __gnu_pbds;
 using namespace std;
-
-int w[101];
-int v[101];
-int dp[101][100001];
+using pii = pair<int, int>;
+using i64 = long long;
+typedef tree<i64, null_type, less<i64>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    // Frational Knapsack - Greedy로 풀이 가능.
-    // 그러나 일반 Kanpsack은 Greedy가 최적해 보장 X
-    // 따라서 dynamic programming or backtracking으로 풀이.
-    
-    // i 번호, j 배낭 무게
-    // 부분집합에 포함되지 않는 경우 DP[i-1][j]
-    // 부분집합에 포함하는 경우 DP[i-1][j-W[i]] + V[i]
-    
+    fastio;
+
     int n, k;
-    cin >> n >> k; // n개의 물건, 총 무게 K
-    
-    for (int i = 1; i <= n; ++i) {
-        cin >> w[i] >> v[i]; // i번째 물건의 무게와 가치.
-    }
-    
+    cin >> n >> k;
+    vector<int> w(n+1), v(n+1);
+    for (int i = 1; i <= n; ++i) cin >> w[i] >> v[i];
+//    vector<vector<int>> dp(n+1, vector<int>(k+1));
+//    for (int i = 1; i <= n; ++i) {
+//        for (int j = 1; j <= k; ++j) {
+//            if (w[i] > j) dp[i][j] = dp[i-1][j];
+//            else dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
+//        }
+//    }
+//    cout << dp[n][k];
+
+    vector<vector<int>> dp(2, vector<int>(k+1));
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= k; ++j) {
-            if (j >= w[i]) dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
-            else dp[i][j] = dp[i-1][j];
+            if (w[i] > j) dp[1][j] = dp[0][j];
+            else dp[1][j] = max(dp[0][j], dp[0][j-w[i]] + v[i]);
         }
+        swap(dp[0], dp[1]);
     }
-    
-    cout << dp[n][k];
+    cout << dp[0][k];
 }
