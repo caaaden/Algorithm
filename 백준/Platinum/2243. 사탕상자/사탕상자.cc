1a44d143fb282b611036ee3280a4cf300f17b024
caaaden <121105930+caaaden@users.noreply.github.com>
2024-11-01 17:39:41 +0900

    [Platinum V] Title: 사탕상자, Time: 68 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.cc
+++ 백준/Platinum/2243. 사탕상자/사탕상자.cc
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
-using ll = long long;
 
-void update(vector<ll>& tree, int node, int start, int end, int index, ll val) {
+const int MAX = 1'000'000;
+vector<int> tree;
+void update(int node, int start, int end, int index, int val) {
     if (index < start || index > end) return;
     if (start == end) {
         tree[node] += val;
         return;
     }
-    update(tree, node*2, start, (start+end)/2, index, val);
-    update(tree, node*2+1, (start+end)/2+1, end, index, val);
+    update(node*2, start, (start+end)/2, index, val);
+    update(node*2+1, (start+end)/2+1, end, index, val);
     tree[node] = tree[node*2] + tree[node*2+1];
 }
 
-ll query(vector<ll>& tree, int node, int start, int end, int x) {
+int query(int node, int start, int end, int x) {
     if (start == end) return end;
-    if (x > tree[node*2]) return query(tree, node*2+1, (start+end)/2+1, end, x-tree[node*2]);
-    else return query(tree, node*2, start, (start+end)/2, x);
+    if (x > tree[node*2]) return query(node*2+1, (start+end)/2+1, end, x-tree[node*2]);
+    else return query(node*2, start, (start+end)/2, x);
 }
 
 int main() {
@@ -25,21 +26,20 @@ int main() {
 
     int n;
     cin >> n;
-    const int MAX = 1'000'000;
-    vector<ll> tree(MAX*4);
+    tree.resize(MAX*4);
     while (n--) {
         int op;
         cin >> op;
         if (op == 1) {
             int x;
             cin >> x;
-            int t = query(tree, 1, 1, MAX, x);
-            update(tree, 1, 1, MAX, t, -1);
+            int t = query(1, 1, MAX, x);
+            update(1, 1, MAX, t, -1);
             cout << t << '\n';
         } else {
             int a, b;
             cin >> a >> b;
-            update(tree, 1, 1, MAX, a, b);
+            update(1, 1, MAX, a, b);
         }
     }
 }
