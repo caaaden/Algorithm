2be68420a27b0633e100d3cff92d1981580fb44f
caaaden <121105930+caaaden@users.noreply.github.com>
2024-11-04 14:45:43 +0900

    [Gold III] Title: ABB to BA (Hard), Time: 24 ms, Memory: 3620 KB -BaekjoonHub

--- 백준/Gold/32293. ABB to BA （Hard）/ABB to BA （Hard）.cc
+++ 백준/Gold/32293. ABB to BA （Hard）/ABB to BA （Hard）.cc
@@ -17,19 +17,11 @@ int main() {
             s2 += c;
             int cnt = 0;
             while (s2.size() >= 3) {
-                string s3;
-                s3 += s2.back(); s2.pop_back();
-                s3 += s2.back(); s2.pop_back();
-                s3 += s2.back(); s2.pop_back();
-                if (s3 == "BBA") {
+                if (s2.substr(s2.size()-3) == "ABB") {
+                    for (int i = 0; i < 3; ++i) s2.pop_back();
                     s2 += 'B';
                     cnt++;
-                } else {
-                    s2 += s3.back(); s3.pop_back();
-                    s2 += s3.back(); s3.pop_back();
-                    s2 += s3.back(); s3.pop_back();
-                    break;
-                }
+                } else break;
             }
             for (int i = 0; i < cnt; ++i) s2 += 'A';
         }
