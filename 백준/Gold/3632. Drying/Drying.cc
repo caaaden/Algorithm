35350bc4f15ab2564d48917082a98fa28f79c1c8
caaaden <121105930+caaaden@users.noreply.github.com>
2024-11-30 22:47:11 +0900

    [Gold V] Title: Drying, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/3632. Drying/Drying.cc
+++ 백준/Gold/3632. Drying/Drying.cc
@@ -12,19 +12,17 @@ using piii = pair<int, pii>;
 int main() {
     fastio;
 
-    // FF...FT...TT
-    // lo = 0, hi = 1e9
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> v(n);
+    vector<int> v(n);
     for (auto& e : v) cin >> e;
-    ll k;
+    int k;
     cin >> k;
 
-    auto check = [&](ll x) {
+    auto check = [&](int x) {
         ll timeTaken = 0;
         for (auto e : v) {
-            e = max(e-x, 0LL);
+            e = max(e-x, 0);
             if (e) {
                 if (k == 1) return false;
                 timeTaken += e / (k-1);
@@ -34,14 +32,11 @@ int main() {
         return timeTaken <= x;
     };
 
-    ll lo = 0, hi = 1e9+10;
+    int lo = 0, hi = 1e9;
     while (lo + 1 < hi) {
-        ll mid = (lo + hi) / 2;
-        if (check(mid)) {
-            hi = mid;
-        } else {
-            lo = mid;
-        }
+        int mid = (lo + hi) / 2;
+        if (check(mid)) hi = mid;
+        else lo = mid;
     }
     cout << lo+1;
 }
