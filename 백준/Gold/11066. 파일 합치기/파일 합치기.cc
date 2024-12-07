6d1644beb01fdf0a17e83241251381f8b8a54dcb
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-07 20:45:58 +0900

    [Gold III] Title: 파일 합치기, Time: 4 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -1,49 +1,111 @@
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0), cin.tie(0);
-#define all(v) v.begin(), v.end()
-#define X first
-#define Y second
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using pil = pair<int, ll>;
-using piii = pair<int, pii>;
+#define size 100005
+int w[size]; /* node weights */
+int l[size], r[size]; /* left and right children */
+int d[size]; /* depth */
+int q[size]; /* working region */
+int v[size]; /* number of node in working region */
+int t; /* current size of working region */
+int m; /* current node */
+
+/*Phase 1 : Function builds a rooted binary tree having the values
+ as leaves but possibly in the wrong order*/
+void combine(int k) {
+	int j, d, x;
+	m++;
+	l[m] = v[k - 1];
+	r[m] = v[k];
+	w[m] = x = q[k - 1] + q[k];
+	t--;
+	for (j = k; j <= t; j++)
+		q[j] = q[j + 1], v[j] = v[j + 1];
+	for (j = k - 2; q[j] < x; j--)
+		q[j + 1] = q[j], v[j + 1] = v[j];
+	q[j + 1] = x;
+	v[j + 1] = m;
+	while (j > 0 && q[j - 1] <= x) {
+		d = t - j;
+		combine(j);
+		j = t - d;
+	}
+}
+
+/*Phase 2 : Function finds the depth of the each node and
+stores it in the depth array */
+void mark(int k, int p) {
+	d[k] = p;
+	if (l[k] >= 0)
+		mark(l[k], p + 1);
+	if (r[k] >= 0)
+		mark(r[k], p + 1);
+}
+
+/*Phase 3: Function reorders the leaf nodes
+in the same order as of the original sequence given*/
+void build(int b) {
+	int j = m;
+	if (d[t] == b)	l[j] = t++;
+	else {
+		m--;
+		l[j] = m;
+		build(b + 1);
+	}
+	if (d[t] == b)	r[j] = t++;
+	else {
+		m--;
+		r[j] = m;
+		build(b + 1);
+	}
+}
 
 int main() {
-    fastio;
 
-    // Knuth's Opt
-    int t;
-    cin >> t;
-    while (t--) {
+    int T;
+    cin >> T;
+    while (T--) {
         int n;
-        cin >> n;
-        vector<int> pSum(n);
-        cin >> pSum[0];
-        for (int i = 1; i < n; ++i) {
-            int x;
-            cin >> x;
-            pSum[i] = pSum[i-1] + x;
+        cin >> n; n--;
+        for (int i = 0; i <= n; i++) {
+            cin >> w[i];
+            l[i] = r[i] = -1;
         }
-        vector<vector<int>> dp(n+1, vector<int>(n+1, INT_MAX));
-        vector<vector<int>> A(n, vector<int>(n));
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = 0;
-            A[i][i] = i;
+        m = n;
+        t = 1;
+        q[0] = 1000000000; /* infinity */
+        q[1] = w[0];
+        v[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            while (q[t - 1] <= w[i])
+                combine(t);
+            t++;
+            q[t] = w[i];
+            v[t] = i;
         }
-        for (int len = 1; len < n; ++len) {
-            for (int i = 0; i+len < n; ++i) {
-                int j = i + len;
-                for (int k = A[i][j-1]; k <= A[i+1][j]; ++k) {
-                    int tmp = dp[i][k] + dp[k+1][j];
-                    if (dp[i][j] > tmp) {
-                        dp[i][j] = tmp;
-                        A[i][j] = k;
-                    }
-                }
-                dp[i][j] += pSum[j] - (i ? pSum[i-1] : 0);
+        while (t > 1)
+            combine(t);
+        mark(v[1], 0);
+        t = 0;
+        m = 2 * n;
+        build(1);
+        for (int i = 0 ; i <= 2 * n; i++) {
+            int ans = 0;
+            if (l[i] != -1) ans += w[l[i]];
+            else ans = w[i];
+            if (r[i] != -1) ans += w[r[i]];
+            else ans = w[i];
+            w[i] = ans;
+        }
+        for (int i = 2 * n ; i >= 0 ; i--) {
+            if (r[i] != -1) {
+                d[r[i]] = d[i] + 1;
+            }
+            if (l[i] != -1) {
+                d[l[i]] = d[i] + 1;
             }
         }
-        cout << dp[0][n-1] << '\n';
+        int sum = 0;
+        for (int i = n+1; i <= 2*n; ++i) sum += w[i];
+        cout << sum << '\n';
     }
 }
