fc554433e05e7cab5b2a4aecd4a4d5313e4c9a83
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-07 19:15:47 +0900

    [Gold III] Title: 파일 합치기, Time: 16 ms, Memory: 4124 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -24,7 +24,7 @@ int main() {
             cin >> x;
             pSum[i] = pSum[i-1] + x;
         }
-        vector<vector<int>> dp(n+3, vector<int>(n+3, INT_MAX));
+        vector<vector<int>> dp(n+1, vector<int>(n+1, INT_MAX));
         vector<vector<int>> A(n, vector<int>(n));
         for (int i = 0; i < n; ++i) {
             dp[i][i] = 0;
