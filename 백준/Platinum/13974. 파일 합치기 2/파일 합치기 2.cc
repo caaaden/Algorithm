3c4f2146c8931b604846f1a05a769dfaa987c41c
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-07 21:14:23 +0900

    [Platinum II] Title: 파일 합치기 2, Time: 20 ms, Memory: 2620 KB -BaekjoonHub

--- 백준/Platinum/13974. 파일 합치기 2/파일 합치기 2.cc
+++ 백준/Platinum/13974. 파일 합치기 2/파일 합치기 2.cc
@@ -1,49 +1,84 @@
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0), cin.tie(0);
-#define all(v) v.begin(), v.end()
-#define X first
-#define Y second
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using pil = pair<int, ll>;
-using piii = pair<int, pii>;
+#define size 25555
+int w[size];
+int l[size], r[size];
+int d[size];
+int q[size];
+int v[size];
+int t;
+int m;
+
+void combine(int k) {
+	int i, tmp, x;
+	m++;
+	l[m] = v[k - 1];
+	r[m] = v[k];
+	w[m] = x = q[k - 1] + q[k];
+	t--;
+	for (i = k; i <= t; i++) q[i] = q[i + 1], v[i] = v[i + 1];
+	for (i = k - 2; q[i] < x; i--) q[i + 1] = q[i], v[i + 1] = v[i];
+	q[i + 1] = x;
+	v[i + 1] = m;
+	while (i > 0 && q[i - 1] <= x) {
+        tmp = t - i;
+		combine(i);
+        i = t - tmp;
+	}
+}
+
+void mark(int k, int p) {
+	d[k] = p;
+	if (l[k] >= 0) mark(l[k], p + 1);
+	if (r[k] >= 0) mark(r[k], p + 1);
+}
+
+void build(int b) {
+	int j = m;
+	if (d[t] == b) l[j] = t++;
+	else {
+		m--;
+		l[j] = m;
+		build(b+1);
+	}
+	if (d[t] == b) r[j] = t++;
+	else {
+		m--;
+		r[j] = m;
+		build(b+1);
+	}
+}
 
 int main() {
-    fastio;
 
-    // Knuth's Opt
-    int t;
-    cin >> t;
-    while (t--) {
+    int T;
+    cin >> T;
+    while (T--) {
         int n;
-        cin >> n;
-        vector<int> pSum(n);
-        cin >> pSum[0];
-        for (int i = 1; i < n; ++i) {
-            int x;
-            cin >> x;
-            pSum[i] = pSum[i-1] + x;
+        cin >> n; n--;
+        for (int i = 0; i <= n; i++) {
+            cin >> w[i];
+            l[i] = r[i] = -1;
         }
-        vector<vector<int>> dp(n+1, vector<int>(n+1, INT_MAX));
-        vector<vector<int>> A(n, vector<int>(n));
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = 0;
-            A[i][i] = i;
+        m = n;
+        t = 1;
+        q[0] = 1e9;
+        q[1] = w[0];
+        v[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            while (q[t - 1] <= w[i]) combine(t);
+            t++;
+            q[t] = w[i];
+            v[t] = i;
         }
-        for (int len = 1; len < n; ++len) {
-            for (int i = 0; i+len < n; ++i) {
-                int j = i + len;
-                for (int k = A[i][j-1]; k <= A[i+1][j]; ++k) {
-                    int tmp = dp[i][k] + dp[k+1][j];
-                    if (dp[i][j] > tmp) {
-                        dp[i][j] = tmp;
-                        A[i][j] = k;
-                    }
-                }
-                dp[i][j] += pSum[j] - (i ? pSum[i-1] : 0);
-            }
-        }
-        cout << dp[0][n-1] << '\n';
+        while (t > 1) combine(t);
+        mark(v[1], 0);
+        t = 0;
+        m = n*2;
+        build(1);
+        for (int i = 0; i <= n*2; i++) if (l[i] != -1 && r[i] != -1) w[i] = w[l[i]] + w[r[i]];
+        int sum = 0;
+        for (int i = n+1; i <= 2*n; ++i) sum += w[i];
+        cout << sum << '\n';
     }
 }
