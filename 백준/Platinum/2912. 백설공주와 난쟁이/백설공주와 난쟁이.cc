6be5f4df527345affab9ed24e7c4be6660a3424a
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-12 16:22:12 +0900

    [Platinum II] Title: 백설공주와 난쟁이, Time: 56 ms, Memory: 5664 KB -BaekjoonHub

--- 백준/Platinum/2912. 백설공주와 난쟁이/백설공주와 난쟁이.cc
+++ 백준/Platinum/2912. 백설공주와 난쟁이/백설공주와 난쟁이.cc
@@ -19,11 +19,10 @@ int main() {
     int n, c;
     cin >> n >> c;
     vector<int> v(n+1);
-//    vector<int> indices[10001]; // 어떤 숫자의 인덱스들
-    vector<vector<int>> indices(c+1);
+    vector<int> indices[10001]; // 어떤 숫자의 인덱스들
     for (int i = 1; i <= n; ++i) {
         cin >> v[i];
-//        if (indices[v[i]].empty()) indices[v[i]].push_back(-1); // 검색 편의를 위해 삽입
+        if (indices[v[i]].empty()) indices[v[i]].push_back(-1); // 검색 편의를 위해 삽입
         indices[v[i]].push_back(i);
     }
     int m;
@@ -31,22 +30,18 @@ int main() {
     while (m--) {
         int l, r;
         cin >> l >> r;
-        // l...r 에서 랜덤으로 하나 고르기, 그걸 100번했을 때 과반이 있다면 걸리지 않을 확률 1/2^100
         uniform_int_distribution<int> dis(l, r);
-        // dis(mt)
-        // indices, l, r
-        int found = -1;
-        for (int i = 0; i < 100; ++i) {
+        int found = 0;
+        for (int i = 0; i < 30; ++i) {
             int idx = dis(mt);
             int val = v[idx];
-            // indices[val]
             int cnt = upper_bound(all(indices[val]), r) - lower_bound(all(indices[val]), l);
             if (cnt > (r - l + 1) / 2) {
                 found = val;
                 break;
             }
         }
-        if (found != -1) cout << "yes " << found << '\n';
+        if (found) cout << "yes " << found << '\n';
         else cout << "no" << '\n';
     }
 }
