531ac00bd83fb8b66bd97b671e0ef5c0dae955aa
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-24 13:54:51 +0900

    [Gold I] Title: 구간 합 구하기, Time: 180 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,48 +1,31 @@
 #include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
 using namespace std;
 using ll = long long;
 
-void init(vector<ll>& v, vector<ll>& tree, int node, int start, int end) {
-    if (start == end) {
-        tree[node] = v[start];
-        return;
-    }
-    init(v, tree, node*2, start, (start+end)/2);
-    init(v, tree, node*2+1, (start+end)/2+1, end);
-    tree[node] = tree[node*2] + tree[node*2+1];
-}
-
-void update(vector<ll>& v, vector<ll>& tree, int node, int start, int end, int index, ll val) {
-    if (index < start || index > end) return;
-    if (start == end) {
-        v[index] = val;
-        tree[node] = val;
-        return;
-    }
-    update(v, tree, node*2, start, (start+end)/2, index, val);
-    update(v, tree, node*2+1, (start+end)/2+1, end, index, val);
-    tree[node] = tree[node*2] + tree[node*2+1];
-}
-
-ll query(vector<ll>& tree, int node, int start, int end, int left, int right) {
-    if (left <= start && end <= right) return tree[node];
-    if (end < left || start > right) return 0;
-    ll lSum = query(tree, node*2, start, (start+end)/2, left, right);
-    ll rSum = query(tree, node*2+1, (start+end)/2+1, end, left, right);
-    return lSum + rSum;
-}
-
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    fastio;
 
     int n, m, k;
     cin >> n >> m >> k;
-    vector<ll> v(n+1);
-    vector<ll> tree(n*4);
-    for (int i = 1; i <= n; ++i) cin >> v[i];
-    init(v, tree, 1, 1, n);
+    vector<ll> t(n<<1);
+    for (int i = n; i < (n<<1); ++i) cin >> t[i];
+    auto build = [&]() {
+        for (int i = n-1; i; --i) t[i] = t[i<<1] + t[i<<1|1];
+    };
+    auto modify = [&](int p, ll val) {
+        for (t[p += n] = val; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+    };
+    auto query = [&](int l, int r) {
+        ll res = 0;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l&1) res += t[l++];
+            if (r&1) res += t[--r];
+        }
+        return res;
+    };
     m += k;
+    build();
     while (m--) {
         int a;
         cin >> a;
@@ -50,11 +33,11 @@ int main() {
             int b;
             ll c;
             cin >> b >> c;
-            update(v, tree, 1, 1, n, b, c);
+            modify(b-1, c);
         } else {
             int b, c;
             cin >> b >> c;
-            cout << query(tree, 1, 1, n, b, c) << '\n';
+            cout << query(b-1, c) << '\n';
         }
     }
 }
