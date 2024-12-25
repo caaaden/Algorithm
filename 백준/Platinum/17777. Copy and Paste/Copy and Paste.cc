47b88609fa146bba6fc907d331fdc66399d5a996
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-25 22:46:21 +0900

    [Platinum I] Title: Copy and Paste, Time: 9348 ms, Memory: 13652 KB -BaekjoonHub

---
+++ 백준/Platinum/17777. Copy and Paste/Copy and Paste.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
+#define all(v) v.begin(), v.end()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+
+int main() {
+    fastio;
+
+    int m;
+    cin >> m;
+    string s;
+    cin >> s;
+    crope r(s.c_str());
+    int n;
+    cin >> n;
+    while (n--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        r.insert(c, r.substr(a, b-a));
+        if (r.size() > m) r.erase(m, r.size()-m);
+    }
+    cout << r;
+}
