667a03aa07a44fd34f9dd14bf0113bc6bfceef7c
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-26 09:28:23 +0900

    [Gold IV] Title: 돌 그룹, Time: 264 ms, Memory: 16296 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
+#define all(v) v.begin(), v.end()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+
+int main() {
+    fastio;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    set<vector<int>> st;
+    vector<int> tmp = {a, b, c};
+    sort(all(tmp));
+    st.insert(tmp);
+    queue<vector<int>> Q;
+    Q.push(tmp);
+    while (Q.size()) {
+        vector<int> now = Q.front();
+        Q.pop();
+        if (now[0] == now[1] && now[1] == now[2]) {
+            cout << 1;
+            return 0;
+        }
+        if (now[0] < now[1]) {
+            int x = now[0] * 2;
+            int y = now[1] - now[0];
+            vector<int> tmp2 = {x, y, now[2]};
+            sort(all(tmp2));
+            if (!st.count(tmp2)) {
+                st.insert(tmp2);
+                Q.push(tmp2);
+            }
+        }
+        if (now[0] < now[2]) {
+            int x = now[0] * 2;
+            int y = now[2] - now[0];
+            vector<int> tmp2 = {x, y, now[1]};
+            sort(all(tmp2));
+            if (!st.count(tmp2)) {
+                st.insert(tmp2);
+                Q.push(tmp2);
+            }
+        }
+        if (now[1] < now[2]) {
+            int x = now[1] * 2;
+            int y = now[2] - now[1];
+            vector<int> tmp2 = {x, y, now[0]};
+            sort(all(tmp2));
+            if (!st.count(tmp2)) {
+                st.insert(tmp2);
+                Q.push(tmp2);
+            }
+        }
+    }
+    cout << 0;
+}
