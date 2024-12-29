f4f676d20cd5a1dd1303a0844b95a33e257f5c4a
caaaden <121105930+caaaden@users.noreply.github.com>
2024-12-30 00:23:29 +0900

    [Gold V] Title: Contact, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
+#define all(v) v.begin(), v.end()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+
+int main() {
+    fastio;
+
+    regex pattern("(100+1+|01)+");
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        cout << (regex_match(s, pattern) ? "YES" : "NO") << '\n';
+    }
+}
