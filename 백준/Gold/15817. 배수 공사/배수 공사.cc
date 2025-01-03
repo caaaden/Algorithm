c9ff224a421278ca6ee009299a4ab85c1c41131f
caaaden <121105930+caaaden@users.noreply.github.com>
2025-01-03 09:38:46 +0900

    [Gold IV] Title: 배수 공사, Time: 40 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/15817. 배수 공사/배수 공사.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0);
+#define all(v) v.begin(), v.end()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+
+int main() {
+    fastio;
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> l(n), c(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> l[i] >> c[i];
+    }
+    vector<int> dp(x+1);
+    dp[0] = 1;
+    for (int i = 0; i < n; ++i) {
+        int L = l[i];
+        int C = c[i];
+        for (int j = x; j >= L; --j) {
+            int sum = 0;
+            for (int k = 0; k < C; ++k) {
+                sum += L;
+                if (j - sum < 0) break;
+                dp[j] += dp[j - sum];
+            }
+        }
+    }
+    cout << dp[x];
+}
