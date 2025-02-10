d6f393773f214871038dffeef73dd5fb4a5d1b58
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-02-10 15:30:47 +0900

    [Silver II] Title: 이상한 하노이 탑, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.cc
+++ 백준/Silver/15500. 이상한 하노이 탑/이상한 하노이 탑.cc
@@ -36,31 +36,26 @@ int main(){
     cin >> n;
     vector<int> v(n);
     for (auto& e : v) cin >> e;
-    vector<int> vSorted = v;
+    vector<int> vSorted{v};
     sort(rall(vSorted));
-    vector<int> st1, st2;
-    st1 = v;
+    stack<int, vector<int>> st1(v), st2;
     vector<pii> ans;
     for (auto& disk : vSorted) {
-        // stick[1]에서 disk를 찾는다.
-        // 찾았을 경우 stick[2]에 있는 원판들을
-        // 다시 stick[1]으로 옮긴다.
-        // 위를 반복한다.
         while (st1.size()) {
-            if (st1.back() == disk) {
-                st1.pop_back();
+            if (st1.top() == disk) {
+                st1.pop();
                 ans.push_back({1, 3});
                 break;
             } else {
-                st2.push_back(st1.back());
-                st1.pop_back();
+                st2.push(st1.top());
+                st1.pop();
                 ans.push_back({1, 2});
             }
         }
         // 2에서 1로 옮기기
         while (st2.size()) {
-            st1.push_back(st2.back());
-            st2.pop_back();
+            st1.push(st2.top());
+            st2.pop();
             ans.push_back({2, 1});
         }
     }
