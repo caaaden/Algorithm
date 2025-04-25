aa23c242943c0c5aec0c624cba831fa3209ff759
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-04-26 06:29:33 +0900

    [Silver III] Title: 카드놀이, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3231. 카드놀이/카드놀이.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        v[x] = i;
+    }
+    int cnt = 0;
+    for (int i = 1; i < n; ++i) if (v[i] > v[i+1]) ++cnt;
+    cout << cnt;
+}
