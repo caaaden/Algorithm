b646d69aee64d7e4e4aaeefc4e9e1b4e3be8aecd
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-05-01 00:19:37 +0900

    [Silver I] Title: 개구리, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Silver/23797. 개구리/개구리.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    string s;
+    cin >> s;
+    int cur = 0;
+    int mmax = 0, mmin = 0;
+    for (auto& c : s) {
+        if (c == 'K') {
+            ++cur;
+            mmax = max(mmax, cur);
+        } else {
+            --cur;
+            mmin = min(mmin, cur);
+        }
+    }
+    cout << mmax - mmin;
+}
