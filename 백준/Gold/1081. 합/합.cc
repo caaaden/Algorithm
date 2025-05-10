f2bb8921835f723350a73f5f16c032492a651ee7
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-05-10 10:00:57 +0900

    [Gold I] Title: 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1081. 합/합.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    auto powerInt = [](int base, int exp) {
+        ll res = 1;
+        for (int i = 0; i < exp; ++i) res *= base;
+        return res;
+    };
+
+    vector<ll> p(10);
+    for (int i = 1; i < 10; ++i) p[i] = 45 * powerInt(10, i-1) + p[i-1] * 10;
+
+    function<ll(int)> digitSumInRange = [&](int x) {
+        // 1~x-1까지의 각 숫자의 자릿수의 합의 합
+
+        // base case
+        if (x < 10) {
+            // 0...x-1까지의 합
+            ll sum = 0;
+            for (int i = 0; i < x; ++i) sum += i;
+            return sum;
+        }
+        // ...
+
+        int sz = to_string(x).size(); // 자릿수
+        int firstDigit = x / powerInt(10, sz-1); // a
+        ll ret = 0;
+        for (int i = 0; i < firstDigit; ++i) ret += i;
+        ret *= powerInt(10, sz-1);
+        ret += firstDigit * p[sz-1];
+        ll r = x % powerInt(10, sz-1);
+        return ret + firstDigit * r + digitSumInRange(r);
+    };
+
+    int l, u;
+    cin >> l >> u;
+    cout << digitSumInRange(u+1) - digitSumInRange(l);
+}
