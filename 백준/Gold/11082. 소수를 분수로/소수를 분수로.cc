c88f6dd1dbaa34d5bd3e9f9e071543cca5dc74d1
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-05-22 03:15:36 +0900

    [Gold V] Title: 소수를 분수로, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11082. 소수를 분수로/소수를 분수로.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const int MOD = 1e9 + 7;
+// const ll MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    auto powerOf10 = [](int x) {
+        ll ret = 1;
+        for (int i = 0; i < x; ++i) ret *= 10;
+        return ret;
+    };
+
+    string s;
+    cin >> s;
+    int idx = s.find('.');
+    if (idx == -1) {
+        cout << s << "/1";
+        return 0;
+    }
+    int idx2 = s.find('(');
+    if (idx2 == -1) {
+        // ab.cde
+        // 12.345
+        // 12 + 345/1000
+        // 12 * 1000 / 1000 + ...
+        // s.substr(0, idx)
+        string first = s.substr(0, idx);
+        string last = s.substr(idx + 1);
+        ll den = powerOf10(last.size());
+        ll num = stoll(last) + den * stoll(first);
+        ll GCD = gcd(num, den);
+        num /= GCD;
+        den /= GCD;
+        cout << num << '/' << den;
+        return 0;
+    }
+    int idx3 = s.find(')');
+    int x = idx2 - idx - 1;
+    int y = idx3 - idx2 - 1;
+    string a = s.substr(0, idx) + s.substr(idx+1, idx2-idx-1);
+    string b = a + s.substr(idx2+1, idx3-idx2-1);
+    ll num = stoll(b) - stoll(a);
+    ll den = powerOf10(x+y) - powerOf10(x);
+    ll GCD = gcd(num, den);
+    num /= GCD;
+    den /= GCD;
+    cout << num << '/' << den << '\n';
+}
