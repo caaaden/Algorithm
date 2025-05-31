5dbb1bee155bac06ef4b751df2710b662ac0d3a7
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-05-31 21:26:24 +0900

    [Silver I] Title: Split The GSHS 4, Time: 0 ms, Memory: 2024 KB, Score: 6 point -BaekjoonHub

---
+++ 백준/Silver/33811. Split The GSHS 4/Split The GSHS 4.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (n < 4) {
+        cout << 0;
+        return 0;
+    }
+    vector<int> v(n-3);
+    for (int i = 0; i < n-3; ++i) {
+        string ss = s.substr(i, 4);
+        if (ss == "SSHS") v[i] = 1;
+        else if (ss == "GSHS") v[i] = -1;
+    }
+    vector<int> dp(n-3);
+    int ans = dp[0] = v[0];
+    for (int i = 1; i < n-3; ++i) {
+        dp[i] = max(dp[i-1], 0) + v[i];
+        ans = max(ans, dp[i]);
+    }
+    cout << ans;
+}
