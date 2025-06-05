ab2d1991614337207d7d1bcd2ec14472c1cae8e7
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-06-05 18:29:24 +0900

    [Silver V] Title: 욱제는 결벽증이야!!, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/15887. 욱제는 결벽증이야！！/욱제는 결벽증이야！！.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto& e : v) cin >> e;
+    vector<pii> ans;
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i; j < n; ++j) {
+            if (v[j] == i+1) {
+                if (i == j) break;
+                ans.push_back({i+1, j+1});
+                reverse(v.begin()+i, v.begin()+j+1);
+                break;
+            }
+        }
+    }
+    cout << ans.size() << '\n';
+    for (auto& [a, b]: ans) cout << a << ' ' << b << '\n';
+}
