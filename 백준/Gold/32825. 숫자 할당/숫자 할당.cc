949dc17822dc5a03f696a3a626652bd5d5b70e74
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-06-08 00:47:37 +0900

    [Gold IV] Title: 숫자 할당, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/32825. 숫자 할당/숫자 할당.cc
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    vector<vector<int>> board(5, vector<int>(5));
+    for (int i = 1; i < 5; ++i) cin >> board[0][i];
+    for (int i = 1; i < 5; ++i) cin >> board[i][0];
+    int ans = 0;
+    vector<int> vis(14);
+    vector<pii> base = {{2, 2}, {2, 3}, {2, 4}, {3, 2}, {3, 3}, {4, 2}};
+
+    auto isNotInRange = [](int x) {
+        return x < 1 || x > 13;
+    };
+
+    auto fillAndCheck = [&]() {
+        board[1][2] = board[0][2] - (board[2][2] + board[3][2] + board[4][2]);
+        if (isNotInRange(board[1][2])) return false;
+        board[1][3] = board[0][3] - (board[2][3] + board[3][3]);
+        if (isNotInRange(board[1][3])) return false;
+        board[1][4] = board[0][4] - board[2][4];
+        if (isNotInRange(board[1][4])) return false;
+        board[2][1] = board[2][0] - (board[2][2] + board[2][3] + board[2][4]);
+        if (isNotInRange(board[2][1])) return false;
+        board[3][1] = board[3][0] - (board[3][2] + board[3][3]);
+        if (isNotInRange(board[3][1])) return false;
+        board[4][1] = board[4][0] - board[4][2];
+        if (isNotInRange(board[4][1])) return false;
+        board[1][1] = board[1][0] - (board[1][2] + board[1][3] + board[1][4]);
+        if (isNotInRange(board[1][1])) return false;
+        if (board[1][1] != board[0][1] - (board[2][1] + board[3][1] + board[4][1])) return false;
+//        unordered_set<int> us;
+//        if (vis[board[1][1]]) return false;
+//        us.insert(board[1][1]);
+//        for (int i = 2; i <= 4; ++i) {
+//            if (vis[board[1][i]] || vis[board[i][1]]) return false;
+//            us.insert(board[1][i]);
+//            us.insert(board[i][1]);
+//        }
+//        return us.size() == 7;
+        vector<bool> check(14);
+        for (int i = 1; i <= 4; ++i) {
+            for (int j = 1; j <= 4; ++j) {
+                if ((i == 3 && j == 4) || (i == 4 && j == 3)) break;
+                if (check[board[i][j]]) return false;
+                check[board[i][j]] = true;
+            }
+        }
+        return true;
+    };
+
+    function<void(int)> dfs = [&](int d) {
+        if (d == 6) {
+            ans += fillAndCheck();
+            return;
+        }
+        for (int i = 1; i <= 13; ++i) {
+            if (vis[i]) continue;
+            vis[i] = 1;
+            board[base[d].X][base[d].Y] = i;
+            dfs(d+1);
+            board[base[d].X][base[d].Y] = 0;
+            vis[i] = 0;
+        }
+    };
+
+    dfs(0);
+    cout << ans;
+}
