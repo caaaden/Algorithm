71131a7a4ebf76c51e7fc2039d5a94e36dea6a77
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-05 16:37:04 +0900

    [Platinum V] Title: 연세워터파크, Time: 20 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Platinum/15678. 연세워터파크/연세워터파크.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    int n, d; cin >> n >> d;
+    priority_queue<pli> pq;
+    ll ans = LLONG_MIN;
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        while (pq.size() && pq.top().Y < i-d) pq.pop();
+        ll now = x + max((pq.size() ? pq.top().X : 0ll), 0ll);
+        ans = max(ans, now);
+        pq.push({now, i});
+    }
+    cout << ans;
+}
