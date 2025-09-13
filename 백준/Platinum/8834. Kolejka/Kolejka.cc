a299424d618fded3a8200d8eb868a3f5808cc2b8
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-14 02:22:04 +0900

    [Platinum V] Title: Kolejka, Time: 128 ms, Memory: 7616 KB -BaekjoonHub

---
+++ 백준/Platinum/8834. Kolejka/Kolejka.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int> v(n+1);
+        ordered_set<int> os;
+        for (int i = 1; i <= n; ++i) {
+            cin >> v[i];
+            os.insert(i);
+        }
+        vector<int> ans(n+1);
+        for (int i = n; i >= 1; --i) {
+            auto it = os.find_by_order(v[i]);
+            ans[i] = *it;
+            os.erase(it);
+        }
+        for (int i = 1; i <= n; ++i) cout << ans[i] << ' ';
+        cout << '\n';
+    }
+}
