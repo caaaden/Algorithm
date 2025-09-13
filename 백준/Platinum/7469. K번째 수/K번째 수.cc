da2837d47ffd747e1379bf8ce1401ca64646d31d
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-13 19:18:01 +0900

    [Platinum II] Title: K번째 수, Time: 112 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Platinum/7469. K번째 수/K번째 수.cc
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    int n, m; cin >> n >> m;
+    vector<vector<int>> t(n<<1);
+    for (int i = n; i < (n<<1); ++i) {
+        int x; cin >> x;
+        t[i].push_back(x);
+    }
+
+    auto build = [&]() {
+        for (int i = n-1; i; --i) {
+            t[i].resize(t[i<<1].size()+t[i<<1|1].size());
+            merge(all(t[i<<1]), all(t[i<<1|1]), t[i].begin());
+        }
+    };
+
+    auto query = [&](int l, int r, int k) {
+        int tmpL = l;
+        int tmpR = r;
+        int lo = -1e9-1, hi = 1e9+1;
+        // init
+//        for (tmpL += n, tmpR += n; tmpL < tmpR; tmpL >>= 1, tmpR >>= 1) {
+//            if (tmpL&1) {
+//                // t[l]
+//                lo = min(lo, t[tmpL].front());
+//                hi = max(hi, t[tmpL].back());
+//                tmpL++;
+//            }
+//            if (tmpR&1) {
+//                tmpR--;
+//                // t[r]
+//                lo = min(lo, t[tmpR].front());
+//                hi = max(hi, t[tmpR].back());
+//            }
+//        }
+
+        auto check = [&](int x) {
+            tmpL = l;
+            tmpR = r;
+            int idx = 0;
+            for (tmpL += n, tmpR += n; tmpL < tmpR; tmpL >>= 1, tmpR >>= 1) {
+                if (tmpL&1) {
+                    // t[l]
+                    idx += lower_bound(all(t[tmpL]), x) - t[tmpL].begin();
+                    tmpL++;
+                }
+                if (tmpR&1) {
+                    tmpR--;
+                    // t[r]
+                    idx += lower_bound(all(t[tmpR]), x) - t[tmpR].begin();
+                }
+            }
+            idx++;
+            return idx <= k;
+        };
+
+//        cout << lo << ' ' << hi << '\n';
+        while (lo + 1 < hi) {
+            int mid = (lo + hi) / 2;
+            if (check(mid)) lo = mid;
+            else hi = mid;
+        }
+        return lo;
+    };
+
+    build();
+//    for (auto& e : t[1]) cout << e << ' ';
+//    cout << '\n';
+    while (m--) {
+        int i, j, k;
+        cin >> i >> j >> k;
+        cout << query(i-1, j, k) << '\n';
+    }
+}
