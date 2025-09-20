c8be51f12772e045024d0be2f818c2fd3e470454
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-20 20:32:06 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.cc
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    int n; cin >> n;
+    vector<vector<int>> dp(n+1, vector<int>(10, -1));
+    for (int i = 1; i <= n; ++i) {
+        int m; cin >> m;
+        for (int j = 0; j < m; ++j) {
+            int x; cin >> x;
+            dp[i][x] = 0;
+        }
+    }
+    for (int i = 1; i < 10; ++i) {
+        if (dp[1][i] == 0) dp[1][i] = 1;
+    }
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j < 10; ++j) {
+            if (dp[i][j] == -1) continue;
+            for (int k = 1; k < 10; ++k) {
+                if (dp[i-1][k] == -1) continue;
+                if (j == k) continue;
+                dp[i][j] |= dp[i-1][k];
+            }
+        }
+    }
+    int check = 0;
+    for (int i = 1; i < 10; ++i) {
+        if (dp[n][i] == -1) continue;
+        check |= dp[n][i];
+    }
+    if (check == 0) {
+        cout << -1;
+        return 0;
+    }
+    vector<int> ans;
+    int prev = 0;
+    for (int i = n; i >= 1; --i) {
+        for (int j = 1; j < 10; ++j) {
+            if (dp[i][j] < 1) continue;
+            if (prev == j) continue;
+            ans.push_back(j);
+            prev = j;
+            break;
+        }
+    }
+    reverse(all(ans));
+    for (auto& e : ans) cout << e << '\n';
+}
