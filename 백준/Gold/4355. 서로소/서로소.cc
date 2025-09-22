f81707efce58b1abb0f7df6014e82650ac7ed49c
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-23 00:22:54 +0900

    [Gold I] Title: 서로소, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/4355. 서로소/서로소.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+
+int main(){
+    fastio;
+
+    auto getPrimeFactors = [](int x) {
+        map<int, int> factors;
+        for (int i = 2; i*i <= x; ++i) {
+            while (x % i == 0) {
+                factors[i]++;
+                x /= i;
+            }
+        }
+        if (x > 1) factors[x]++;
+        return factors;
+    };
+    auto power = [](int a, int b) {
+        int ret = 1;
+        for (int i = 0; i < b; ++i) ret *= a;
+        return ret;
+    };
+    while (true) {
+        int n;
+        cin >> n;
+        if (!n) break;
+        if (n == 1) {
+            cout << 0 << '\n';
+            continue;
+        }
+        map<int, int> factors = getPrimeFactors(n);
+        int ans = 1;
+        for (auto& [a, b] : factors) ans *= power(a, b-1) * (a-1);
+        cout << ans << '\n';
+    }
+}
