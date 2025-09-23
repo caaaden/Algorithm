6c35820cac7ed9198405e7f3f5317a987ce0c9e8
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-24 04:20:23 +0900

    [Gold V] Title: 카드게임, Time: 116 ms, Memory: 17960 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/10835. 카드게임/카드게임.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    int n; cin >> n;
+    vector<int> a(n+1), b(n+1);
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+    for (int i = 1; i <= n; ++i) cin >> b[i];
+    vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+
+    function<int(int, int)> solve = [&](int x, int y) {
+        if (x > n || y > n) return 0;
+        if (dp[x][y] != -1) return dp[x][y];
+        int& ret = dp[x][y];
+        ret = max(solve(x+1, y), solve(x+1, y+1));
+        if (a[x] > b[y]) ret = max(ret, solve(x, y+1) + b[y]);
+        return ret;
+    };
+
+    cout << solve(1, 1);
+}
