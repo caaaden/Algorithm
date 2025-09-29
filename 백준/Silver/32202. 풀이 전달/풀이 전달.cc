227ed18a62dcb5c8daefa8d6f932cbfab3641461
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-30 06:35:28 +0900

    [Silver I] Title: 풀이 전달, Time: 76 ms, Memory: 56744 KB -BaekjoonHub

--- 백준/Silver/32202. 풀이 전달/풀이 전달.cc
+++ 백준/Silver/32202. 풀이 전달/풀이 전달.cc
@@ -39,12 +39,11 @@ int main(){
     fastio;
 
     int n; cin >> n;
-    vector<vector<ll>> dp(n+1, vector<ll>(3));
-    dp[1][0] = 1, dp[1][1] = 1, dp[1][2] = 1;
-    for (int i = 1; i < n; ++i) {
-        dp[i+1][0] = (dp[i][0] + dp[i][1] + dp[i][2]) % MOD;
-        dp[i+1][1] = (dp[i][0] + dp[i][1] + dp[i][2]) % MOD;
-        dp[i+1][2] = (dp[i][0] + dp[i][1]) % MOD;
+    vector<vector<int>> dp(n+1, vector<int>(2));
+    dp[1][0] = 2, dp[1][1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        dp[i][0] = (((dp[i-1][0] + dp[i-1][1]) % MOD) << 1) % MOD;
+        dp[i][1] = dp[i-1][0];
     }
-    cout << (dp[n][0] + dp[n][1] + dp[n][2]) % MOD;
+    cout << (dp[n][0] + dp[n][1]) % MOD;
 }
