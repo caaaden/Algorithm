9b37b7fe250de52e2aeb4f32fa3232de05f4c659
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-09-30 05:14:06 +0900

    [Silver I] Title: 풀이 전달, Time: 76 ms, Memory: 56744 KB -BaekjoonHub

---
+++ 백준/Silver/32202. 풀이 전달/풀이 전달.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    int n; cin >> n;
+    vector<vector<ll>> dp(n+1, vector<ll>(3));
+    dp[1][0] = 1, dp[1][1] = 1, dp[1][2] = 1;
+    for (int i = 1; i < n; ++i) {
+        dp[i+1][0] = (dp[i][0] + dp[i][1] + dp[i][2]) % MOD;
+        dp[i+1][1] = (dp[i][0] + dp[i][1] + dp[i][2]) % MOD;
+        dp[i+1][2] = (dp[i][0] + dp[i][1]) % MOD;
+    }
+    cout << (dp[n][0] + dp[n][1] + dp[n][2]) % MOD;
+}
