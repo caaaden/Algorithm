246fdfc8790d8a67e79e03963da5f1ad6e21d553
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-10-09 09:44:33 +0900

    [Silver II] Title: 알파벳 블록, Time: 92 ms, Memory: 7380 KB -BaekjoonHub

---
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int bit = 9;
+const long long MOD1 = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+    
+    deque<char> dq;
+    stack<int> st;
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int op; cin >> op;
+        if (op == 1) {
+            char c; cin >> c;
+            dq.push_back(c);
+            st.push(1);
+        } else if (op == 2) {
+            char c; cin >> c;
+            dq.push_front(c);
+            st.push(2);
+        } else {
+            if (st.empty()) continue;
+            if (st.top() == 1) {
+                dq.pop_back();
+            } else {
+                dq.pop_front();
+            }
+            st.pop();
+        }
+    }
+    if (dq.empty()) {
+        cout << 0;
+        return 0;
+    }
+    for (auto& e : dq) cout << e;
+}
