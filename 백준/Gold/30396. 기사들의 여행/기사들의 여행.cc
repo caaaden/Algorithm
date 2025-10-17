671100b7670871b8da07b6e268dc241cd5b92e02
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-10-17 19:32:31 +0900

    [Gold IV] Title: 기사들의 여행, Time: 4 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/30396. 기사들의 여행/기사들의 여행.cc
+++ 백준/Gold/30396. 기사들의 여행/기사들의 여행.cc
@@ -41,88 +41,55 @@ const int MAX = 1e9;
 int main(){
     fastio;
 
-    // 4x4 board를 숫자로 16bits 숫자로 표현
-    // 숫자로 방문 표시 및 거리 표시
-    // 큐에도 숫자로 삽입 및 pop
-    // 상태 변환 시에만 board로 변환 후 가능한 이동 경우의 수를 모두 확인
-    // vector<vector<bool>> board
-    // int b
-    vector<vector<bool>> s(4, vector<bool>(4));
-    vector<vector<bool>> e(4, vector<bool>(4));
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            char c; cin >> c;
-            s[i][j] = c - '0';
-        }
-    }
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            char c; cin >> c;
-            e[i][j] = c - '0';
-        }
-    }
-    // boardToNumber : 보드 -> 숫자 변환
-    // boardToNumber(vector<vector<bool>>& b)
-    auto boardToNumber = [](vector<vector<bool>>& b) {
-        int ret = 0;
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                ret |= b[i][j];
-                ret <<= 1;
-            }
-        }
-        ret >>= 1;
-        return ret;
+    auto getCoord = [](int bit) {
+        return make_pair(bit >> 2, bit & 3);
     };
 
-    auto numberToBoard = [](int x) {
-        vector<vector<bool>> ret(4, vector<bool>(4));
-        for (int i = 3; i >= 0; --i) {
-            for (int j = 3; j >= 0; --j) {
-                // x & 1
-                ret[i][j] = x & 1;
-                x >>= 1;
-            }
-        }
-        return ret;
+    auto getBit = [](int x, int y) {
+        return (x << 2) | y;
     };
 
+    int s = 0, e = 0;
+    for (int i = 0; i < 16; ++i) {
+        char c; cin >> c;
+        if (c == '1') s |= (1 << i);
+    }
+    for (int i = 0; i < 16; ++i) {
+        char c; cin >> c;
+        if (c == '1') e |= (1 << i);
+    }
     vector<int> dist(100000, -1);
-    dist[boardToNumber(s)] = 0;
-    // queue<int> Q;
+    dist[s] = 0;
     queue<int> Q;
-    Q.push(boardToNumber(s));
+    Q.push(s);
     while (Q.size()) {
-        vector<vector<bool>> nowB = numberToBoard(Q.front());
-        Q.pop();
-        if (nowB == e) {
-            cout << dist[boardToNumber(e)];
+        int now = Q.front(); Q.pop();
+        if (now == e) {
+            cout << dist[e];
             return 0;
         }
-        // nowB[i][j] == true인 (i, j)에 대해
-        // 이동할 수 있는 모든 곳
-        // 범위를 벗어날 경우 : continue
-        // 이미 다른 1이 있을 경우 : continue
-        // 이미 방문한 경우 : continue
-        // dist + 1, Q.push
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                if (!nowB[i][j]) continue;
+        for (int i = 0; i < 16; ++i) {
+            // now & (1 << i)
+            if (now & (1 << i)) {
+                // i -> (x, y)
+                auto [x, y] = getCoord(i);
+                // x + dx2[dir]
                 for (int dir = 0; dir < 8; ++dir) {
-                    int nx = i + dx2[dir];
-                    int ny = j + dy2[dir];
-                    // 0...3
+                    int nx = x + dx2[dir];
+                    int ny = y + dy2[dir];
                     if (nx < 0 || nx > 3 || ny < 0 || ny > 3) continue;
-                    if (nowB[nx][ny]) continue;
-                    // (i, j) -> (nx, ny)
-                    // swap(nowB[i][j], nowB[nx][ny])
-                    // nowB[i][j] = 0, nowB[nx][ny] = 1
-                    // dist[boardToNumber(nowB)] != -1
-                    vector<vector<bool>> nextB = nowB;
-                    swap(nextB[i][j], nextB[nx][ny]);
-                    if (dist[boardToNumber(nextB)] != -1) continue;
-                    dist[boardToNumber(nextB)] = dist[boardToNumber(nowB)] + 1;
-                    Q.push(boardToNumber(nextB));
+                    int pos = getBit(nx, ny);
+                    // now & (1 << bit)
+                    if (now & (1 << pos)) continue;
+                    // i -> bit
+                    // i를 끄고 bit를 켜기
+                    int next = now;
+                    next &= ~(1 << i);
+                    next |= (1 << pos);
+                    // next가 방문하지 않았는 지 확인
+                    if (dist[next] != -1) continue;
+                    dist[next] = dist[now] + 1;
+                    Q.push(next);
                 }
             }
         }
