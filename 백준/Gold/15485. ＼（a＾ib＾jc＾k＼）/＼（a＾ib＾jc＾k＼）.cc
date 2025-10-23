11ba2c9edce72089e99241950f258446a4ec24be
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-10-23 15:04:49 +0900

    [Gold III] Title: \(a^ib^jc^k\), Time: 76 ms, Memory: 57760 KB -BaekjoonHub

---
+++ 백준/Gold/15485. ＼（a＾ib＾jc＾k＼）/＼（a＾ib＾jc＾k＼）.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int dx2[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+constexpr int dy2[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main(){
+    fastio;
+
+    string s; cin >> s;
+    int n = s.size();
+    vector<vector<ll>> dp(n, vector<ll>(3));
+    // dp[a][b] : a번째, b = 0 : a, b = 1 : ab, b = 2 : abc
+    if (s[0] == 'a') dp[0][0] = 1;
+    for (int i = 1; i < n; ++i) {
+        if (s[i] == 'c') {
+            dp[i][2] = (2*dp[i-1][2] + dp[i-1][1]) % MOD;
+            dp[i][1] = dp[i-1][1];
+            dp[i][0] = dp[i-1][0];
+        } else if (s[i] == 'b') {
+            dp[i][1] = (2*dp[i-1][1] + dp[i-1][0]) % MOD;
+            dp[i][2] = dp[i-1][2];
+            dp[i][0] = dp[i-1][0];
+        } else {
+            dp[i][0] = (2*dp[i-1][0] + 1) % MOD;
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-1][2];
+        }
+    }
+    // dp[n-1][2]
+    cout << dp[n-1][2];
+}
