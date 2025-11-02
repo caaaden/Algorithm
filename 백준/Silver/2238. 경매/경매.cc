3e26f22caf3730267ec9c4941e8a5f4ea45fb8b3
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-11-02 21:35:14 +0900

    [Silver V] Title: 경매, Time: 12 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/2238. 경매/경매.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int dx2[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+constexpr int dy2[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main() {
+    fastio;
+    
+    int u, n; cin >> u >> n;
+    vector<psi> people(u+1);
+    for (int i = 0; i < n; ++i) {
+        string name;
+        int price;
+        cin >> name >> price;
+        if (!people[price].Y) {
+            people[price].X = name;
+        }
+        people[price].Y++;
+    }
+    int cnt = MAX;
+    int auctionPrice;
+    string winner;
+    for (int i = 1; i <= u; ++i) {
+        if (!people[i].Y) continue;
+        if (people[i].Y < cnt) {
+            cnt = people[i].Y;
+            auctionPrice = i;
+            winner = people[i].X;
+        }
+    }
+    cout << winner << ' ' << auctionPrice;
+}
