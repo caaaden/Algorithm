ce262b09fb5c32d5408f49d53b47f9fc8b2ab9e9
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-11-11 22:48:44 +0900

    [Silver II] Title: 트리플렛, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3042. 트리플렛/트리플렛.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int dx2[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+constexpr int dy2[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main() {
+    fastio;
+
+    int n; cin >> n;
+    vector<pii> pos;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            char c; cin >> c;
+            if (c == '.') continue;
+            pos.push_back({i, j});
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < pos.size()-2; ++i) {
+        for (int j = i+1; j < pos.size()-1; ++j) {
+            for (int k = j+1; k < pos.size(); ++k) {
+                int diffX1 = pos[i].X - pos[j].X;
+                int diffY1 = pos[i].Y - pos[j].Y;
+                int GCD = gcd(diffX1, diffY1);
+                int a = diffX1 / GCD;
+                int b = diffY1 / GCD;
+                int diffX2 = pos[j].X - pos[k].X;
+                int diffY2 = pos[j].Y - pos[k].Y;
+                bool isLine = diffX2 * b == diffY2 * a;
+                ans += isLine;
+            }
+        }
+    }
+    cout << ans;
+}
