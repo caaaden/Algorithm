df8b506a23cac14e14e6e15233cf2a1b44e9be48
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-11-17 02:27:03 +0900

    [Bronze I] Title: 멀티버스 Ⅰ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18868. 멀티버스 Ⅰ/멀티버스 Ⅰ.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int dx2[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+constexpr int dy2[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main() {
+    fastio;
+    
+    int m, n; cin >> m >> n;
+    vector<vector<int>> spaces(m, vector<int>(n));
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> spaces[i][j];
+        }
+    }
+
+    auto isEqual = [&n](vector<int>& a, vector<int>& b) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j < n; ++j) {
+                if (a[i] < a[j] && b[i] < b[j]) continue;
+                if (a[i] == a[j] && b[i] == b[j]) continue;
+                if (a[i] > a[j] && b[i] > b[j]) continue;
+                return false;
+            }
+        }
+        return true;
+    };
+
+    int ans = 0;
+    for (int i = 0; i < m; ++i) {
+        for (int j = i+1; j < m; ++j) {
+            ans += isEqual(spaces[i], spaces[j]);
+        }
+    }
+    cout << ans;
+}
