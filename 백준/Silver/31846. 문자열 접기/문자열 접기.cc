45c657ebb8dc31bc1e2be3d966ef684c117847bc
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-11-23 19:21:58 +0900

    [Silver IV] Title: 문자열 접기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/31846. 문자열 접기/문자열 접기.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int dx2[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+constexpr int dy2[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
+
+int main() {
+    fastio;
+
+    int n; cin >> n;
+    string s; cin >> s;
+    int q; cin >> q;
+    while (q--) {
+        int l, r; cin >> l >> r;
+        l--, r--;
+        string s2 = s.substr(l, r-l+1);
+        int ans = 0;
+        for (int i = 0; i < s2.size()-1; ++i) {
+            int cnt = 0;
+            for (int j = 0; i-j >= 0 && i+1+j <= s2.size()-1; ++j) {
+                cnt += s2[i-j] == s2[i+1+j];
+            }
+            ans = max(ans, cnt);
+        }
+        cout << ans << endl;
+    }
+}
