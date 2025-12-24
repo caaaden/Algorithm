f87ea8c0b6c7112a388a9f24ef5408294bf522df
Minjun Kim <121105930+caaaden@users.noreply.github.com>
2025-12-24 17:00:04 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 192 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,54 +1,53 @@
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-using namespace std;
-using ll = long long;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
 
-template<const bool negative = true, const int BITS = 9, typename _RandomAccessIterator>
-inline void radix_sort(_RandomAccessIterator _first, _RandomAccessIterator _last, const bool reverse = false)
-{
-    typedef typename iterator_traits<_RandomAccessIterator>::value_type value_type;
-    if(negative)
-    {
-        if(!reverse) {
-            _RandomAccessIterator _middle = partition(_first, _last, [](value_type a) -> bool {return a<0;});
-            for(auto it = _first; it != _middle; it++)*it *= -1;
-            radix_sort<false>(_first, _middle, true);
-            for(auto it = _first; it != _middle; it++)*it *= -1;
-            radix_sort<false>(_middle, _last);
-        } else {
-            _RandomAccessIterator _middle = partition(_first, _last, [](value_type a) -> bool {return a>=0;});
-            radix_sort<false>(_first, _middle, true);
-            for(auto it = _middle; it != _last; it++)*it *= -1;
-            radix_sort<false>(_middle, _last);
-            for(auto it = _middle; it != _last; it++)*it *= -1;
-        }
-        return;
-    }
-    constexpr int PART = (1<<BITS);
-    constexpr int FULL = (1<<BITS)-1;
-    int shift = 0;
-    auto MAX = *max_element(_first, _last);
-    vector<value_type> P[PART];
-    while(log2(MAX) >= shift)
-    {
-        for(auto it = _first; it != _last; it++)P[(((*it)>>shift)&FULL)^(reverse?FULL:0)].push_back(*it);
-        auto it = _first;
-        for(int i = 0; i < PART; i++)
-        {
-            for(auto u: P[i]) *it++ = u;
-            P[i].clear();
-        }
-        shift += BITS;
-    }
-}
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define X first
+#define Y second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using psi = pair<string, int>;
+using piii = pair<int, pii>;
+using piiii = pair<pii, pii>;
+
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename Compare = less<T>>
+using ordered_set = tree<T, null_type, Compare, rb_tree_tag, tree_order_statistics_node_update>;
+
+constexpr int dx[4] = {1, -1, 0, 0};
+constexpr int dy[4] = {0, 0, 1, -1};
+constexpr int dx2[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+constexpr int dy2[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+constexpr int bit = 9;
+const long long MOD = 1e9 + 7;
+const long long MOD2 = 1e9 + 9;
+const int MAX = 1e9;
 
 int main() {
     fastio;
 
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (auto& e : v) cin >> e;
-    radix_sort(v.begin(), v.end());
-    for (auto& e : v) cout << e << '\n';
+    vector<bool> v(2000001);
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        v[x + 1000000] = true;
+    }
+    for (int i = 0; i <= 2000000; ++i) {
+        if (v[i]) cout << i - 1000000 << endl;
+    }
 }
